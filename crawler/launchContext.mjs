f80dae2d516b3b839de4b86cdd68795168dc431f
Serdar Ashirov <webapis@users.noreply.github.com>
2023-06-25 10:33:41 +0500

    Update launchContext.mjs

--- crawler/launchContext.mjs
+++ crawler/launchContext.mjs
@@ -3,19 +3,19 @@ export default {
     // If it doesn't, feel free to remove this.
     // useChrome: true,
     launchOptions: {
-        protocolTimeout: 240000,
+    
       //  headless: process.env.HEADLESS === 'true' ? true : false,
          args: ['--no-sandbox', '--disable-setuid-sandbox', "--disable-web-security",
             `--window-size=1200,1250`,
-            "--allow-insecure-localhost",
-            //  "--user-data-dir=/tmp/foo",
-            "--ignore-certificate-errors",
-            '--disable-gpu-rasterization',
-            '--disable-low-res-tiling',
-            '--disable-skia-runtime-opts',
-            '--disable-yuv420-biplanar',
-            '--disable-site-isolation-trials',
-            '--disable-dev-shm-usage',
+            // "--allow-insecure-localhost",
+            // //  "--user-data-dir=/tmp/foo",
+            // "--ignore-certificate-errors",
+            // '--disable-gpu-rasterization',
+            // '--disable-low-res-tiling',
+            // '--disable-skia-runtime-opts',
+            // '--disable-yuv420-biplanar',
+            // '--disable-site-isolation-trials',
+            // '--disable-dev-shm-usage',
             //'--lang=en-US,en'
             // '--shm-size=3gb'
 
