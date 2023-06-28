1db8e9b4a06cc5649cc2d79e860c5ce738dc7760
Serdar Ashirov <webapis@users.noreply.github.com>
2023-06-28 20:20:33 +0500

    Update sementa.js

--- urls/biraradamoda/kadin/sementa.js
+++ urls/biraradamoda/kadin/sementa.js
@@ -1,8 +1,8 @@
 const urls = [
- // { url: 'https://www.sementa.com/kadin-ceket' }
-  //  { url: 'https://www.sementa.com/kadin-aksesuar' },
-  //     { url: 'https://www.sementa.com/kadin-giyim' },
-  //     { url: 'https://www.sementa.com/buyuk-beden' },
+ { url: 'https://www.sementa.com/kadin-ceket' },
+   { url: 'https://www.sementa.com/kadin-aksesuar' },
+      { url: 'https://www.sementa.com/kadin-giyim' },
+      { url: 'https://www.sementa.com/buyuk-beden' },
 ]
 
 module.exports = { urls }
