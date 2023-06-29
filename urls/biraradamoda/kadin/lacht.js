eb1b9560baabd2b9805fa305e8f4892675f386c5
Serdar Ashirov <webapis@users.noreply.github.com>
2023-06-29 18:07:52 +0500

    lcwaikiki

--- urls/biraradamoda/kadin/lacht.js
+++ urls/biraradamoda/kadin/lacht.js
@@ -1,11 +1,11 @@
 const urls = [
     
-    { url: 'https://www.lacht.com.tr/alt-giyim'},
-    { url: 'https://www.lacht.com.tr/ust-giyim'},
-    { url: 'https://www.lacht.com.tr/en-yeniler'},
-    { url: 'https://www.lacht.com.tr/cok-satanlar'},
-    { url: 'https://www.lacht.com.tr/elbise'},
-    { url: 'https://www.lacht.com.tr/aksesuar'},
+    // { url: 'https://www.lacht.com.tr/alt-giyim'},
+    // { url: 'https://www.lacht.com.tr/ust-giyim'},
+    // { url: 'https://www.lacht.com.tr/en-yeniler'},
+    // { url: 'https://www.lacht.com.tr/cok-satanlar'},
+    // { url: 'https://www.lacht.com.tr/elbise'},
+    // { url: 'https://www.lacht.com.tr/aksesuar'},
 
 
 ]
