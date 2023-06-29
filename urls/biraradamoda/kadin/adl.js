03ed7f4e1618ad54f7f0f511160453f0442e79d1
Serdar Ashirov <webapis@users.noreply.github.com>
2023-06-29 17:19:04 +0500

    Update adl.js

--- urls/biraradamoda/kadin/adl.js
+++ urls/biraradamoda/kadin/adl.js
@@ -1,17 +1,17 @@
 const urls = [
   
-    { url: 'https://www.adl.com.tr/kadin-elbise',   subcategory:['elbise'], category: 'üst-giyim' ,   node:'dream' },
-    { url: 'https://www.adl.com.tr/kadin-bluz', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www.adl.com.tr/kadin-pantolon',   subcategory:['elbise'], category: 'üst-giyim' ,   node:'dream' },
-    { url: 'https://www.adl.com.tr/kadin-ceket', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www.adl.com.tr/kadin-gomlek',   subcategory:['elbise'], category: 'üst-giyim' ,   node:'dream' },
-    { url: 'https://www.adl.com.tr/kadin-etek', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www.adl.com.tr/bikini-mayo', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www.adl.com.tr/kadin-sort', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
+    // { url: 'https://www.adl.com.tr/kadin-elbise',   subcategory:['elbise'], category: 'üst-giyim' ,   node:'dream' },
+    // { url: 'https://www.adl.com.tr/kadin-bluz', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
+    // { url: 'https://www.adl.com.tr/kadin-pantolon',   subcategory:['elbise'], category: 'üst-giyim' ,   node:'dream' },
+    // { url: 'https://www.adl.com.tr/kadin-ceket', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
+    // { url: 'https://www.adl.com.tr/kadin-gomlek',   subcategory:['elbise'], category: 'üst-giyim' ,   node:'dream' },
+    // { url: 'https://www.adl.com.tr/kadin-etek', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
+    // { url: 'https://www.adl.com.tr/bikini-mayo', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
+    // { url: 'https://www.adl.com.tr/kadin-sort', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
 
 
-    { url: 'https://www.adl.com.tr/kadin-ayakkabi', subcategory: ['aksesuar'], category: 'üst-giyim', node: 'dream' },
-    { url: 'https://www.adl.com.tr/kadin-canta-aksesuar', subcategory: ['aksesuar'], category: 'üst-giyim', node: 'dream' },
+    // { url: 'https://www.adl.com.tr/kadin-ayakkabi', subcategory: ['aksesuar'], category: 'üst-giyim', node: 'dream' },
+    { url: 'https://www.adl.com.tr/kadin-canta-aksesuar' },
 
 ]
 
