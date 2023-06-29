e11b6e1f639be6b1bec80432bd6a823af95fc835
Serdar Ashirov <webapis@users.noreply.github.com>
2023-06-29 22:06:47 +0500

    Update importData.mjs

--- typesense/importData.mjs
+++ typesense/importData.mjs
@@ -35,8 +35,8 @@ debugger
           imageUrl: m.imageUrl,
           price: m.priceNew ? mapPrice(m.priceNew.toString()) : 0,
 
-          kategori: ['çapraz,bel,sırt,omuz,spor,outdoor'].find((f)=>m.title.includes(f))?['çapraz,bel,sırt,omuz,spor,outdoor'].find((f)=>m.title.includes(f)):'diger',
-          renk: ['sarı,beyaz,kırmızı,siyah,fuşya,turuncu','yeşil','mavi','kahve'].find((f)=>m.title.includes(f))?['sarı,beyaz,kırmızı,siyah,fuşya,turuncu','yeşil','mavi','kahve'].find((f)=>m.title.includes(f)):'diger',
+          kategori: ['çapraz','bel','sırt','omuz','spor','outdoor'].find((f)=>m.title.includes(f))?['çapraz','bel','sırt','omuz','spor','outdoor'].find((f)=>m.title.includes(f)):'diger',
+          renk: ['sarı','beyaz','kırmızı','siyah','fuşya','turuncu','yeşil','mavi','kahve'].find((f)=>m.title.includes(f))?['sarı','beyaz','kırmızı','siyah','fuşya','turuncu','yeşil','mavi','kahve'].find((f)=>m.title.includes(f)):'diger',
           altKategori:'depricated'
         };
       })
