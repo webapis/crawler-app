7e93cca72de4bf118a60fae1a79755bd3b3dbcbb
Serdar Ashirov <webapis@users.noreply.github.com>
2023-06-29 16:50:15 +0500

    bb

--- typesense/createSchema.mjs
+++ typesense/createSchema.mjs
@@ -64,5 +64,5 @@ let schema = {
     ],
     // default_sorting_field: "index",
   };
-  const rest =await client.collections().create(schema);
+ // const rest =await client.collections().create(schema);
   console.log('create schema',rest)
