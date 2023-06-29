7b3c11518372317b894ff7908a4a2782ad2c4531
Serdar Ashirov <webapis@users.noreply.github.com>
2023-06-29 17:34:08 +0500

    defac

--- urls/biraradamoda/kadin/dagi.js
+++ urls/biraradamoda/kadin/dagi.js
@@ -1,14 +1,14 @@
 const urls = [
 
-    { url: 'https://www.dagi.com.tr/kadin/kadin-elbise-modelleri', subcategory: ['elbise'], category: 'üst-giyim',node:'dream' },
-    { url: 'https://www.dagi.com.tr/kadin/kadin-pantolon-modelleri', subcategory: ['elbise'], category: 'alt-giyim',node:'dream' },
-    //{ url: 'https://www.dagi.com.tr/kadin/kadin-elbise-modelleri', subcategory: ['elbise'], category: 'üst-giyim',node:'dream',fn:'elbise' },
+    // { url: 'https://www.dagi.com.tr/kadin/kadin-elbise-modelleri', subcategory: ['elbise'], category: 'üst-giyim',node:'dream' },
+    // { url: 'https://www.dagi.com.tr/kadin/kadin-pantolon-modelleri', subcategory: ['elbise'], category: 'alt-giyim',node:'dream' },
+    // //{ url: 'https://www.dagi.com.tr/kadin/kadin-elbise-modelleri', subcategory: ['elbise'], category: 'üst-giyim',node:'dream',fn:'elbise' },
 
-    { url: 'https://www.dagi.com.tr/kadin/kadin-sweatshirts-modelleri', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
+    // { url: 'https://www.dagi.com.tr/kadin/kadin-sweatshirts-modelleri', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
    
-    { url: 'https://www.dagi.com.tr/kadin/kadin-t-shirt-modelleri', subcategory: ['tişört'], category: 'üst-giyim', node: 'dream' },
+    // { url: 'https://www.dagi.com.tr/kadin/kadin-t-shirt-modelleri', subcategory: ['tişört'], category: 'üst-giyim', node: 'dream' },
 
-    { url: 'https://www.dagi.com.tr/kadin/kadin-sort-modelleri', subcategory: ['şort'], category: 'alt-giyim', node: 'dream' },
+    // { url: 'https://www.dagi.com.tr/kadin/kadin-sort-modelleri', subcategory: ['şort'], category: 'alt-giyim', node: 'dream' },
 
 ]
 
--- urls/biraradamoda/kadin/defacto.js
+++ urls/biraradamoda/kadin/defacto.js
@@ -1,12 +1,12 @@
 const urls = [
+   { url: 'https://www.defacto.com.tr/kadin-canta'},
+//    { url: 'https://www.defacto.com.tr/kadin-giyim'},
+//  //{ url: 'https://www.defacto.com.tr/kadin-jean-etek'}
+//    { url: 'https://www.defacto.com.tr/kadin-ayakkabi' },
 
-   { url: 'https://www.defacto.com.tr/kadin-giyim'},
- //{ url: 'https://www.defacto.com.tr/kadin-jean-etek'}
-   { url: 'https://www.defacto.com.tr/kadin-ayakkabi' },
 
 
-
-    { url: 'https://www.defacto.com.tr/kadin-aksesuar'},
+//     { url: 'https://www.defacto.com.tr/kadin-aksesuar'},
 
 
 ]
