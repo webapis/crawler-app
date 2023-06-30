892a6f67b603954d13e5e719afade51b66abc493
Serdar Ashirov <webapis@users.noreply.github.com>
2023-06-30 12:31:02 +0500

    Update tergan.js

--- urls/biraradamoda/kadin/tergan.js
+++ urls/biraradamoda/kadin/tergan.js
@@ -1,13 +1,13 @@
 const urls = [
 
-    { url: 'https://www.tergan.com.tr/kadin-ayakkabi'},  
-    { url: 'https://www.tergan.com.tr/kadin-cizme'},  
+    // { url: 'https://www.tergan.com.tr/kadin-ayakkabi'},  
+    // { url: 'https://www.tergan.com.tr/kadin-cizme'},  
     { url: 'https://www.tergan.com.tr/kadin-canta'},  
-    { url: 'https://www.tergan.com.tr/kadin-deri-mont'},  
-    { url: 'https://www.tergan.com.tr/kadin-aksesuar'},  
-    { url: 'https://www.tergan.com.tr/kadin-babet'},  
-    { url: 'https://www.tergan.com.tr/kadin-bot'},  
-    { url: 'https://www.tergan.com.tr/unisex-aksesuar'},  
+    // { url: 'https://www.tergan.com.tr/kadin-deri-mont'},  
+    // { url: 'https://www.tergan.com.tr/kadin-aksesuar'},  
+    // { url: 'https://www.tergan.com.tr/kadin-babet'},  
+    // { url: 'https://www.tergan.com.tr/kadin-bot'},  
+    // { url: 'https://www.tergan.com.tr/unisex-aksesuar'},  
 
 
 ]
