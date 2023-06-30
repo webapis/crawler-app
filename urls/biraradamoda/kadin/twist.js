18cf9f2df0209849e4722ef8ade6426b5c5cac20
Serdar Ashirov <webapis@users.noreply.github.com>
2023-06-30 12:17:11 +0500

    Update twist.js

--- urls/biraradamoda/kadin/twist.js
+++ urls/biraradamoda/kadin/twist.js
@@ -1,8 +1,8 @@
 const urls = [
-  
-    { url: 'https://www.twist.com.tr/giyim',          subcategory: ['elbise'], category: 'üst-giyim' ,node:'dream'},
-    { url: 'https://www.twist.com.tr/ayakkabi',          subcategory: ['aksesuar'], category: 'üst-giyim' ,node:'dream'},
-    { url: 'https://www.twist.com.tr/aksesuar',          subcategory: ['aksesuar'], category: 'üst-giyim' ,node:'dream'},
+  {url:'https://www.twist.com.tr/aksesuar/kadin-canta-modelleri'}
+    // { url: 'https://www.twist.com.tr/giyim',          subcategory: ['elbise'], category: 'üst-giyim' ,node:'dream'},
+    // { url: 'https://www.twist.com.tr/ayakkabi',          subcategory: ['aksesuar'], category: 'üst-giyim' ,node:'dream'},
+    // { url: 'https://www.twist.com.tr/aksesuar',          subcategory: ['aksesuar'], category: 'üst-giyim' ,node:'dream'},
 ]
 
 module.exports={urls}
