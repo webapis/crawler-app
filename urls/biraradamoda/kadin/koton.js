ccabb05937bb8410f9fddab316e0d437beae4afc
webapis <60083568+webapis@users.noreply.github.com>
2023-07-03 17:05:06 +0500

    Update koton.js

--- urls/biraradamoda/kadin/koton.js
+++ urls/biraradamoda/kadin/koton.js
@@ -1,18 +1,18 @@
 const urls = [
-    { url: 'https://www.koton.com/kadin-giyim'},
+    // { url: 'https://www.koton.com/kadin-giyim'},
 
-    { url: 'https://www.koton.com/kadin-ayakkabi'},
+    // { url: 'https://www.koton.com/kadin-ayakkabi'},
     { url: 'https://www.koton.com/kadin-canta'},
-    { url: 'https://www.koton.com/kadin-parfum'},
-    { url: 'https://www.koton.com/kadin-kemer'},
-    { url: 'https://www.koton.com/kadin-corap'},
-    { url: 'https://www.koton.com/kadin-cuzdan'},
-    { url: 'https://www.koton.com/kadin-taki'},
+    // { url: 'https://www.koton.com/kadin-parfum'},
+    // { url: 'https://www.koton.com/kadin-kemer'},
+    // { url: 'https://www.koton.com/kadin-corap'},
+    // { url: 'https://www.koton.com/kadin-cuzdan'},
+    // { url: 'https://www.koton.com/kadin-taki'},
 
-    { url: 'https://www.koton.com/kadin-sapka'},
-    { url: 'https://www.koton.com/kadin-sac-aksesuarlari'},
-    { url: 'https://www.koton.com/kadin-kozmetik'},
-    { url: 'https://www.koton.com/kadin-sal'},
+    // { url: 'https://www.koton.com/kadin-sapka'},
+    // { url: 'https://www.koton.com/kadin-sac-aksesuarlari'},
+    // { url: 'https://www.koton.com/kadin-kozmetik'},
+    // { url: 'https://www.koton.com/kadin-sal'},
 
 
 ]
