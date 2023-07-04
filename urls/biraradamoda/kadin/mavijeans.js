c0a70228819e48fe6c0cff618a9bb7cc8f53487c
webapis <60083568+webapis@users.noreply.github.com>
2023-07-04 15:10:51 +0500

    mavijeans

--- urls/biraradamoda/kadin/mavijeans.js
+++ urls/biraradamoda/kadin/mavijeans.js
@@ -1,6 +1,7 @@
 const urls = [
+    {url:`https://www.mavi.com/kadin/canta/c/1?q=:relevance:categoryValue:%C3%87anta:subCategoryValue:Has%C4%B1r%20%C3%87anta:subCategoryValue:Spor%20%C3%87antas%C4%B1:subCategoryValue:S%C4%B1rt%20%C3%87antas%C4%B1:subCategoryValue:Omuz%20%C3%87antas%C4%B1:subCategoryValue:%C3%87apraz%20%C3%87anta`},
 //{url:'https://www.mavi.com/kadin/canta/c/1?q=:relevance:categoryValue:%C3%87anta:subCategoryValue:Has%C4%B1r%20%C3%87anta:subCategoryValue:Spor%20%C3%87antas%C4%B1:subCategoryValue:S%C4%B1rt%20%C3%87antas%C4%B1:subCategoryValue:Omuz%20%C3%87antas%C4%B1:subCategoryValue:%C3%87apraz%20%C3%87anta'}
-    { url: 'https://www.mavi.com/kadin/c/1' }
+  //  { url: 'https://www.mavi.com/kadin/c/1' }
 
 ]
 
