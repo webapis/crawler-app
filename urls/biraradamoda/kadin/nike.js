2391f37663b28feb8b57a7e25ec5e21bb1885ca7
webapis <60083568+webapis@users.noreply.github.com>
2023-07-04 16:03:40 +0500

    d

--- urls/biraradamoda/kadin/nike.js
+++ urls/biraradamoda/kadin/nike.js
@@ -1,4 +1,5 @@
 const urls = [
+    {url:"https://www.nike.com/tr/w/kadin-bags-backpacks-5e1x6z9xy71"}
 //     {url:'https://www.nike.com/tr/w/kadin-ayakkabilar-5e1x6zy7ok'},
 //     {url:'https://www.nike.com/tr/w/kadin-giysiler-5e1x6z6ymx6'},
 //     {url:'https://www.nike.com/tr/w/kadin-performance-3k7dgz5e1x6'},
