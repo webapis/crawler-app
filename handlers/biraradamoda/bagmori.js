7cb37a7a9ae488b470616e0002d65f82d66e4363
webapis <60083568+webapis@users.noreply.github.com>
2023-07-05 09:32:17 +0500

    bagmori

---
+++ handlers/biraradamoda/bagmori.js
@@ -0,0 +1,74 @@
+
+
+
+async function handler(page) {
+
+    const url = await page.url()
+
+    debugger;
+
+    await page.waitForSelector('.ProductList')
+  // const totalProducts= await page.evaluate(()=>parseInt(document.querySelector("[for^=sidebar-filter-p-product_type]").innerText.replace(/[^\d]/g,"")))
+    await autoScroll(page);
+
+    debugger
+    const data = await page.$$eval('.ProductItem', (productCards) => {
+        return productCards.map(document => {
+             const priceNew = document.querySelector('.ProductItem__Price').innerText.replace('TL','')
+            const longlink = document.querySelector(".ProductItem__Title.Heading a").href
+             const link = longlink.substring(longlink.indexOf("https://www.bagmori.com/") + 24)
+            const longImgUrl = Array.from(document.querySelectorAll('.ProductItem__Image')).reverse()[0].getAttribute('data-srcset').split(",")[5].trim()
+             const imageUrlshort = longImgUrl.substring(longImgUrl.indexOf("//cdn.shopify.com/") + 18)//https://cdn3.sorsware.com/
+            const title = document.querySelector(".ProductItem__Title.Heading a").innerText
+            return {
+                title: 'bagmori ' + title.replace(/İ/g,'i').toLowerCase(),
+                priceNew,
+                imageUrl: imageUrlshort,
+                link,
+                timestamp: Date.now(),
+                marka: 'bagmori',
+            }
+        })
+    })
+
+
+
+    //----------
+
+    console.log('data length_____', data.length, 'url:', url)
+
+
+
+    return data.map(m=>{return {...m,title:m.title+" _"+process.env.GENDER }})
+}
+
+async function getUrls(page) {
+
+    return { pageUrls: [], productCount: 0, pageLength: 0 }
+}
+
+async function autoScroll(page) {
+    await page.evaluate(async () => {
+
+        const totalProducts= parseInt( document.querySelector("[for^=sidebar-filter-p-product_type]").innerText.replace(/[^\d]/g,""))
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            let inc = 0
+            var timer = setInterval(() => {
+          
+            const totalCollected =document.querySelectorAll(".ProductItem").length
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+                inc = inc + 1
+                if (totalProducts===totalCollected) {
+                  
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 150);
+        });
+    });
+}
+module.exports = { handler, getUrls }
+
---
+++ urls/biraradamoda/kadin/bagmori.js
@@ -0,0 +1,10 @@
+const urls = [
+
+    // { url: 'https://www.abiyefon.com/abiye-modelleri?currency=TL'},  
+    // { url: 'https://www.abiyefon.com/abiye-ayakkabi?currency=TL'},
+
+    { url: 'https://www.bagmori.com/collections/bel-cantasi'},
+
+]
+
+module.exports = { urls }
