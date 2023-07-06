fe5c7b185ca4cfe774b7db3babb106d24d2d8604
webapis <60083568+webapis@users.noreply.github.com>
2023-07-06 17:03:40 +0500

    Update armine.js

--- urls/biraradamoda/kadin/armine.js
+++ urls/biraradamoda/kadin/armine.js
@@ -4,6 +4,9 @@ const urls = [
 
 
     {url:'https://www.armine.com/canta'},
+    {url:'https://www.armine.com/kartlik'},
+    {url:'https://www.armine.com/elcantasi'},
+    {url:'https://www.armine.com/cuzdan'}
 
 ]
 
