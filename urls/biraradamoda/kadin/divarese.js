89ddf703d60ca993abcf88ca7347363f640dfe41
webapis <60083568+webapis@users.noreply.github.com>
2023-07-06 15:09:01 +0500

    Update divarese.js

--- urls/biraradamoda/kadin/divarese.js
+++ urls/biraradamoda/kadin/divarese.js
@@ -1,11 +1,10 @@
 const urls = [
 
      { url: 'https://www.divarese.com.tr/kadin-canta-modelleri-155' },
-
-    
-
-
-
+     { url: 'https://www.divarese.com.tr/postaci-cantasi-1763' },
+     { url: 'https://www.divarese.com.tr/kadin-canta-clutch-57' },
+     { url: 'https://www.divarese.com.tr/kadin-sirt-cantasi-1488' },
+     { url: 'https://www.divarese.com.tr/kadin-cuzdan-58' },
 
 ]
 
