a0cc6d8a3cd85bfbc6a95232dc92ee11b381380b
webapis <60083568+webapis@users.noreply.github.com>
2023-07-07 16:54:51 +0500

    berchka

---
+++ urls/biraradamoda/kadin/bershka.js
@@ -0,0 +1,11 @@
+const urls = [
+  
+    {url:"https://www.bershka.com/tr/kadin/%C3%A7anta---aksesuar/%C3%A7anta-c1010193138.html?tipology=1010618258"}
+   // {url:"https://www.bershka.com/tr/kadin/giyim/elbise-c1010193213.html"}
+    
+
+    ]
+    
+    module.exports={urls}
+    
+    //
