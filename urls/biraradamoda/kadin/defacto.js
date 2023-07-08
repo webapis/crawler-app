d23a9309ebeccd9807a0a789299ce22aed5f9930
Serdar Ashirov <webapis@users.noreply.github.com>
2023-07-08 14:28:24 +0500

    defacto

--- urls/biraradamoda/kadin/defacto.js
+++ urls/biraradamoda/kadin/defacto.js
@@ -1,5 +1,9 @@
 const urls = [
    { url: 'https://www.defacto.com.tr/kadin-canta'},
+   { url: 'https://www.defacto.com.tr/kadin-hasir-aksesuar'},
+   { url: 'https://www.defacto.com.tr/kadin-cuzdan'},
+   { url: 'https://www.defacto.com.tr/kadin-valiz'},
+
 //    { url: 'https://www.defacto.com.tr/kadin-giyim'},
 //  //{ url: 'https://www.defacto.com.tr/kadin-jean-etek'}
 //    { url: 'https://www.defacto.com.tr/kadin-ayakkabi' },
