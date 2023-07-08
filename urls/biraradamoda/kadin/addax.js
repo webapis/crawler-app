24257f732406b6b411243533c69f2c96580509fe
Serdar Ashirov <webapis@users.noreply.github.com>
2023-07-08 13:17:23 +0500

    Update addax.js

--- urls/biraradamoda/kadin/addax.js
+++ urls/biraradamoda/kadin/addax.js
@@ -1,5 +1,6 @@
 const urls = [
      { url: 'https://www.addax.com.tr/aksesuar/canta' },
+     {url:'https://www.addax.com.tr/aksesuar/cuzdan'}
     // { url: 'https://www.addax.com.tr/ust-giyim', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
     // { url: 'https://www.addax.com.tr/alt-giyim', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
     // { url: 'https://www.addax.com.tr/dis-giyim', subcategory: ['elbise'], category: 'üst-giyim', node: 'dream' },
