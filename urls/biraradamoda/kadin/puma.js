e7b5b556acf6dd20177d262c9b71eb7b9e26f954
webapis <60083568+webapis@users.noreply.github.com>
2023-07-08 09:00:33 +0500

    Update puma.js

--- urls/biraradamoda/kadin/puma.js
+++ urls/biraradamoda/kadin/puma.js
@@ -1,6 +1,6 @@
 const urls = [
-{ url: 'https://tr.puma.com/kadin/giyim.html'}
-
+{ url: 'https://tr.puma.com/kadin/aksesuar/canta.html'},
+{ url: 'https://tr.puma.com/kadin/aksesuar/sirt-cantasi.html'}
  ]
  
  module.exports={urls}
