3603eec49395896d4b34a5964dc489e33fbcbc0e
Serdar Ashirov <webapis@users.noreply.github.com>
2023-07-09 15:27:55 +0500

    tergan

--- handlers/biraradamoda/tergan.js
+++ handlers/biraradamoda/tergan.js
@@ -9,21 +9,25 @@ async function handler(page, context) {
 
     const data = await page.$$eval('.item.itemauto', (productCards) => {
         return productCards.map(productCard => {
-
-            const imageUrl = productCard.querySelector('.product img') && productCard.querySelector('.product img').getAttribute('src')
-            const title = productCard.querySelector(".product .description a").innerHTML.trim()
-            const priceNew = productCard.querySelector('.price-sales') ? productCard.querySelector('.price-sales').textContent.trim().replace('TL', '').trim() : productCard.querySelector('.p-value').textContent.trim().replace('TL', '').trim()
-            const longlink = productCard.querySelector(".product .description a").href
-            const link = longlink.substring(longlink.indexOf("https://www.tergan.com.tr/") + 26)
-            //const imageUrlshort = imageUrl && imageUrl.substring(imageUrl.indexOf("https://www.tergan.com.tr/") + 26)
-            return {
-                title: 'tergan ' + title.replace(/İ/g, 'i').toLowerCase(),
-                priceNew,
-                imageUrl,//: imageUrlshort,
-                link,
-                timestamp: Date.now(),
-                marka: 'tergan',
+            try {
+                const imageUrl = productCard.querySelector('.product img') && productCard.querySelector('.product img').getAttribute('src')
+                const title = productCard.querySelector(".product .description a").innerHTML.trim()
+                const priceNew = productCard.querySelector('.price-sales') ? productCard.querySelector('.price-sales').textContent.trim().replace('TL', '').trim() : productCard.querySelector('.p-value').textContent.trim().replace('TL', '').trim()
+                const longlink = productCard.querySelector(".product .description a").href
+                const link = longlink.substring(longlink.indexOf("https://www.tergan.com.tr/") + 26)
+                //const imageUrlshort = imageUrl && imageUrl.substring(imageUrl.indexOf("https://www.tergan.com.tr/") + 26)
+                return {
+                    title: 'tergan ' + title.replace(/İ/g, 'i').toLowerCase(),
+                    priceNew,
+                    imageUrl,//: imageUrlshort,
+                    link,
+                    timestamp: Date.now(),
+                    marka: 'tergan',
+                }  
+            } catch (error) {
+                return {error:error.toString(),content:document.innerHTML}
             }
+        
         }).filter(f => f.imageUrl !== null && f.title.length > 10)
     })
 
@@ -41,23 +45,22 @@ async function handler(page, context) {
 
 async function getUrls(page) {
     const url = await page.url()
-   // await page.waitForSelector('.pager-btn')
-    const hasNextPage = await page.$('.pager-btn')
+    await page.waitForSelector('.cat-total-count')
+    const productCount = await page.evaluate(()=>document.querySelector('.cat-total-count').innerText.replace(/[^\d]/g,''))
+    const totalPages = Math.ceil(productCount / 96)
     const pageUrls = []
-    debugger
-    if(hasNextPage){
-        const totalPages = await page.evaluate(() => Math.max(...Array.from(document.querySelectorAll('.pager-btn')).map(m => m.innerHTML.replace(/[^\d]/g, '')).filter(Number)))
 
-    debugger
-        let pagesLeft = totalPages
-        for (let i = 2; i <= totalPages; i++) {
-            pageUrls.push(`${url}?rpg=` + i)
-            --pagesLeft
-    
-        }
+    let pagesLeft = totalPages
+    for (let i = 2; i <= totalPages; i++) {
+
+
+
+        pageUrls.push(`${url}?rpg=` + i)
+        --pagesLeft
+
+
     }
 
-
-    return { pageUrls, productCount: 0, pageLength: pageUrls.length + 1 }
+    return { pageUrls, productCount, pageLength: pageUrls.length + 1 }
 }
 module.exports = { handler, getUrls }
--- urls/biraradamoda/kadin/tergan.js
+++ urls/biraradamoda/kadin/tergan.js
@@ -2,7 +2,16 @@ const urls = [
 
     // { url: 'https://www.tergan.com.tr/kadin-ayakkabi'},  
     // { url: 'https://www.tergan.com.tr/kadin-cizme'},  
-    { url: 'https://www.tergan.com.tr/kadin-canta'},  
+    { url: 'https://www.tergan.com.tr/kadin-canta'}, 
+    {url:'https://www.tergan.com.tr/kadin-sirt-cantasi'}
+    , 
+    {url:'https://www.tergan.com.tr/kadin-omuz-cantasi'}
+    , 
+    {url:'https://www.tergan.com.tr/kadin-capraz-askili-canta'}
+    , 
+    {url:'https://www.tergan.com.tr/kadin-el-cantasi'}
+    , 
+    {url:'https://www.tergan.com.tr/valiz'}
     // { url: 'https://www.tergan.com.tr/kadin-deri-mont'},  
     // { url: 'https://www.tergan.com.tr/kadin-aksesuar'},  
     // { url: 'https://www.tergan.com.tr/kadin-babet'},  
