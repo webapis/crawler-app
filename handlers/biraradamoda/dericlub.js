8a02973cc606c9305ea645f13f6d2f4853686fe1
Serdar Ashirov <webapis@users.noreply.github.com>
2023-07-09 12:53:06 +0500

    Update dericlub.js

--- handlers/biraradamoda/dericlub.js
+++ handlers/biraradamoda/dericlub.js
@@ -1,8 +1,8 @@
 
 
-const {  RequestQueue } =require('crawlee');
+const { RequestQueue } = require('crawlee');
 async function handler(page, context) {
-    const { request:{userData:{start,detailPage}}  } = context
+    const { request: { userData: { start, detailPage } } } = context
     const requestQueue = await RequestQueue.open();
     const url = await page.url()
     debugger
@@ -20,7 +20,7 @@ async function handler(page, context) {
             //pageUrls.push(`${url}?sayfa=` + i)
             //--pagesLeft
             debugger
-           await requestQueue.addRequest({ url: `${url}?sayfa=` + i,userData:{  start: false}  })
+            await requestQueue.addRequest({ url: `${url}?sayfa=` + i, userData: { start: false } })
         }
 
 
@@ -33,8 +33,8 @@ async function handler(page, context) {
             }).filter(f => f.link !== null)
         })
         for (let url of data) {
-      
-            await requestQueue.addRequest({ url: url.link,userData:{ start: false, detailPage: true } })
+
+            await requestQueue.addRequest({ url: url.link, userData: { start: false, detailPage: true } })
         }
         return []
     }
@@ -44,15 +44,20 @@ async function handler(page, context) {
         await page.waitForSelector('#ProductDetailMain')
         debugger
         const data = await page.evaluate(() => {
-            const renk = document.querySelector('.size_box.selected').innerHTML.replace('/', '-')
-            return [{
-                title: 'dericlub ' + document.querySelector('.ProductName span').textContent.replaceAll('\n', '').toLowerCase() + ' ' + renk,
-                priceNew: document.querySelector('.spanFiyat').textContent.replace('₺', ''),
-                imageUrl: document.querySelector('#imgUrunResim').src,
-                link: location.href,
-                timestamp: Date.now(),
-                marka: 'dericlub',
-            }]
+            try {
+                const renk = document.querySelector('.size_box.selected').innerHTML.replace('/', '-')
+                return [{
+                    title: 'dericlub ' + document.querySelector('.ProductName span').textContent.replaceAll('\n', '').toLowerCase() + ' ' + renk,
+                    priceNew: document.querySelector('.spanFiyat').textContent.replace('₺', ''),
+                    imageUrl: document.querySelector('#imgUrunResim').src,
+                    link: location.href,
+                    timestamp: Date.now(),
+                    marka: 'dericlub',
+                }]
+            } catch (error) {
+                return [{ error: error.toString(), content: document.innerHTML }]
+            }
+
         })
         debugger
         console.log('data length_____', data.length, 'url:', url, process.env.GENDER)
@@ -73,7 +78,7 @@ async function handler(page, context) {
 
         for (let url of data) {
             debugger
-           await requestQueue.addRequest({ url: url.link,userData:{  start: false, detailPage: true}  })
+            await requestQueue.addRequest({ url: url.link, userData: { start: false, detailPage: true } })
         }
         return []
     }
