cace95012e7ec7e59de349492dc7791f1b6ec714
webapis <60083568+webapis@users.noreply.github.com>
2023-07-10 17:18:15 +0500

    Update derimod.js

--- urls/biraradamoda/kadin/derimod.js
+++ urls/biraradamoda/kadin/derimod.js
@@ -2,8 +2,11 @@ const urls = [
 
     // { url: 'https://www.derimod.com.tr/kadin-ayakkabi'},  
     // { url: 'https://www.derimod.com.tr/kadin-deri-ceket'},  
-    { url: 'https://www.derimod.com.tr/kadin-canta-aksesuar'},  
-
+    { url: 'https://www.derimod.com.tr/kadin-omuz-cantasi'},  
+    { url: 'https://www.derimod.com.tr/kadin-capraz-canta'}, 
+    { url: 'https://www.derimod.com.tr/kadin-sirt-cantasi'}, 
+    { url: 'https://www.derimod.com.tr/kadin-clutch-portfoy'}, 
+    { url: 'https://www.derimod.com.tr/kadin-cuzdan'}, 
 
 ]
 
