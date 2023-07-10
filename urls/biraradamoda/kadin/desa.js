4933289d4d380006e20961fddba4c8a9b8f841c6
webapis <60083568+webapis@users.noreply.github.com>
2023-07-10 17:49:05 +0500

    Update desa.js

--- urls/biraradamoda/kadin/desa.js
+++ urls/biraradamoda/kadin/desa.js
@@ -6,7 +6,6 @@ const urls = [
     { url: 'https://www.desa.com.tr/telefon-cantasi' },
     { url: 'https://www.desa.com.tr/kadin-kartlik' },
     { url: 'https://www.desa.com.tr/kadin-canta' },
-    { url: 'https://www.desa.com.tr/kadin-cuzdan' },
    // { url: 'https://www.desa.com.tr/kadin-aksesuar' },
 
 ]
