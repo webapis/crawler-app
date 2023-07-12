5c031018573bf5e9086a73f3b5e8eceda065ad5d
webapis <60083568+webapis@users.noreply.github.com>
2023-07-12 12:33:35 +0500

    sarar

--- handlers/biraradamoda/sarar.js
+++ handlers/biraradamoda/sarar.js
@@ -1,73 +1,140 @@
+const {  RequestQueue ,Dataset} =require('crawlee');
+async function handler(page,context) {
+const {request:{userData:{start}}}=context
 
-const { RequestQueue  } =require ('crawlee');
+const requestQueue = await RequestQueue.open();
+    debugger
+        const url = await page.url()
+    
+        const pageExist = await page.$('.catalogWrapper')
 
-async function handler(page, context) {
-    const requestQueue = await RequestQueue.open();
-    const { request: { userData: { start } } } = context
-
-    const url = await page.url()
-    let data = []
-    await page.waitForSelector('.catalogWrapper')
-    await page.waitForSelector('.ttl-pro')
-
-    const totalProducts = await page.evaluate(() => parseInt(document.querySelector('.ttl-pro').innerText.replace(/[^\d]/g, '')))
-    const collectedProducts = await page.evaluate(() => document.querySelectorAll('#category-list .imgInner').length)
-  
-    if (collectedProducts >= totalProducts) {
-        debugger
-        data = await page.$$eval('.productItem', (productCards) => {
-            return productCards.map(document => {
-                const imageUrl = document.querySelector('.imgInner img').src
-                const title = document.querySelector('a.detailLink [title]').getAttribute('title').trim()
-                const priceNew = document.querySelector('.discount-in-basket-price span') ? document.querySelector('.discount-in-basket-price span').innerText.replace('₺', '').trim() : document.querySelector('.currentPrice').innerText.replace('₺', '').trim()
-                const longlink = document.querySelector('.detailLink').href
-                const link = longlink.substring(longlink.indexOf("https://sarar.com/") + 18)
-                const imageUrlshort = imageUrl && imageUrl.substring(imageUrl.indexOf("https://cdn.sarar.com/") + 22)
-                return {
-                    title: 'sarar ' + title.replace(/İ/g, 'i').toLowerCase(),
-                    priceNew,
-                    imageUrl: imageUrlshort,
-                    link,
-                    timestamp: Date.now(),
-                    marka: 'sarar',
-                }
-            }).filter(f => f.imageUrl !== null && f.title.length > 5)
-        })
-        console.log('data length_____', data.length, 'url:', url, process.env.GENDER)
-
-        debugger
-        console.log("process.env.GENDER ")
-        const mapgender = data.map((m) => {
-            return { ...m, title: m.title + " _" + process.env.GENDER }
-        })
-        return mapgender
-
-    } else {
-        if (start) {
-            const nextUrl = url + `?ps=2`
-  
-            await requestQueue.addRequest({ url: nextUrl, userData: { start: false } })
-            return []
-        } else {
-            const prevPageNumber = parseInt(url.substring(url.indexOf('?ps=') + 4))
-            const nextUrl = url.replace(`?ps=${prevPageNumber}`, `?ps=${prevPageNumber + 1}`)
-       
-            await requestQueue.addRequest({ url: nextUrl, userData: { start: false } })
-            return []
+        if(start){
+          await page.waitForSelector('.catalogWrapper')
+       await requestQueue.addRequest({url:'https://sarar.com/canta?ps=2',userData:{start:false}})
         }
+
+        else{
+     
+          if(pageExist){
+            const productsDataset = await Dataset.open(`products`);
+            const { items: productItems } = await productsDataset.getData();
+            
+            var groupBy = function (xs, key) {
+              return xs.reduce(function (rv, x) {
+                  (rv[x[key]] = rv[x[key]] || []).push(x);
+                  return rv;
+              }, {});
+          };
+          const groupByimageUrl = groupBy(productItems, 'imageUrl')
+          let similarexists =false
+          for (let image in groupByimageUrl) {
+              const curr = groupByimageUrl[image]
+              image
+              debugger
+              if(curr.length>1){
+                  console.log('similar images',image,': ',curr.length)
+                  similarexists=true
+              }
+         
+         
+          }
+          if(!similarexists){
+            const lastPage = parseInt( url.substring(url.indexOf('ps=')+3))
+            await requestQueue.addRequest({url:`https://sarar.com/canta?ps=${lastPage+1}`,userData:{start:false}})
+          }
+
+          }else{
+        
+          }
+
+        }
+        if(start  || pageExist){
+          const data = await page.$$eval('.catalogWrapper .productItem', (productCards) => {
+            return productCards.map(document => {
+              try {
+                   const imageUrl = document.querySelector('span[itemprop="image"]').getAttribute('content')
+                   const title = document.querySelector('span[itemprop="name"]').getAttribute('content')
+                   const priceNew = document.querySelector('.discount-in-basket-price span')? document.querySelector('.discount-in-basket-price span').innerText.replace('₺',''):document.querySelector('.currentPrice').innerText.replace('₺','').trim()
+                   const link = document.querySelector('span[itemprop="url"]').getAttribute('content')
+
+                  return {
+                       title: 'sarar ' + title.replace(/İ/g,'i').toLowerCase(),
+                       priceNew,
+                       imageUrl,
+                       link,
+                       timestamp: Date.now(),
+                       marka: 'sarar',
+                  }
+              } catch (error) {
+                return {error:error.toString(),content:document.innerHTML}
+              }
+             
+            })//.filter(f => f.imageUrl !== null && f.title.length > 5)
+        })
+    debugger
+        console.log('data length_____', data.length, 'url:', url,process.env.GENDER)
+    
+    
+        console.log("process.env.GENDER ")
+        // const formatprice = data.map((m) => {
+        //     return { ...m, title: m.title + " _" + process.env.GENDER }
+        // })
+    
+    
+        return data
+        } else{
+          []
+        }
+
+   
     }
-
-
-
-
-
-
-
-}
-
-async function getUrls(page) {
-
-
-    return { pageUrls:[], productCount: 0, pageLength: 0 }
-}
-module.exports = { handler, getUrls }
+    async function autoScroll(page) {
+        page.on("console", (message) => {
+          console.log("Message from Puppeteer page:", message.text());
+        });
+        await page.evaluate(async () => {
+          await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            let inc = 0;
+      
+            var timer = setInterval(() => {
+              var scrollHeight = document.body.scrollHeight;
+      
+              window.scrollBy(0, distance);
+              totalHeight += distance;
+              inc = inc + 1;
+              console.log("inc", inc);
+              if (totalHeight >= scrollHeight - window.innerHeight) {
+                if (inc === 50) {
+                  clearInterval(timer);
+                  resolve();
+                }
+              } else {
+                inc = 0;
+              }
+            }, 500);
+          });
+        });
+      }
+    async function getUrls(page) {
+        // const url = await page.url()
+        // await page.waitForSelector('.catalog__meta--product-count span')
+        // const productCount = await page.$eval('.catalog__meta--product-count span', element => parseInt(element.innerHTML))
+        // const totalPages = Math.ceil(productCount / 60)
+        const pageUrls = []
+    
+        // let pagesLeft = totalPages
+        // for (let i = 2; i <= totalPages; i++) {
+    
+    
+    
+        //     pageUrls.push(`${url}?page=` + i)
+        //     --pagesLeft
+    
+    
+        // }
+    
+        return { pageUrls, productCount:0, pageLength: pageUrls.length + 1 }
+    }
+    module.exports = { handler, getUrls }
