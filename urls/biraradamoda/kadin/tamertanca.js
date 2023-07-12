fcd38e9c3b8d68ba2ec8ce71127acfb4467c007b
webapis <60083568+webapis@users.noreply.github.com>
2023-07-12 12:47:15 +0500

    Update tamertanca.js

--- urls/biraradamoda/kadin/tamertanca.js
+++ urls/biraradamoda/kadin/tamertanca.js
@@ -1,6 +1,6 @@
 const urls = [
       { url: 'https://www.tamertanca.com.tr/canta-kadin' },
-
+      { url: 'https://www.tamertanca.com.tr/aksesuar-valiz' },
     ]
     
     module.exports = { urls }
