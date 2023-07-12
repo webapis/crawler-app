889a46a7bde379676d50d0f0cc88b48cce0b0272
webapis <60083568+webapis@users.noreply.github.com>
2023-07-12 10:36:36 +0500

    Update addax.js

--- handlers/biraradamoda/addax.js
+++ handlers/biraradamoda/addax.js
@@ -13,20 +13,25 @@ async function handler(page) {
     debugger
     const data = await page.$$eval('.Prd', (productCards) => {
         return productCards.map(document => {
-            const priceNew = Array.from(document.querySelector('.SalesAmount').querySelectorAll('.PPrice')).reverse()[0].innerHTML.replace('TL', '').trim()
-            const longlink = document.querySelector('a[data-product').href
-            const link = longlink.substring(longlink.indexOf("https://www.addax.com.tr/") + 25)
-            const longImgUrl = document.querySelector("img[data-src]").getAttribute('data-src')
-            const imageUrlshort = longImgUrl.substring(longImgUrl.indexOf("https://cdn3.sorsware.com/") + 26)//https://cdn3.sorsware.com/
-            const title = document.querySelector("img[data-src]").alt
-            return {
-                title: 'addax ' + title.replace(/İ/g,'i').toLowerCase(),
-                priceNew,
-                imageUrl: imageUrlshort,
-                link,
-                timestamp: Date.now(),
-                marka: 'addax',
+            try {
+                const priceNew = Array.from(document.querySelector('.SalesAmount').querySelectorAll('.PPrice')).reverse()[0].innerHTML.replace('TL', '').trim()
+                const longlink = document.querySelector('a[data-product').href
+                const link = longlink.substring(longlink.indexOf("https://www.addax.com.tr/") + 25)
+                const longImgUrl = document.querySelector("img[data-src]").getAttribute('data-src')
+                const imageUrlshort = longImgUrl.substring(longImgUrl.indexOf("https://cdn3.sorsware.com/") + 26)//https://cdn3.sorsware.com/
+                const title = document.querySelector("img[data-src]").alt
+                return {
+                    title: 'addax ' + title.replace(/İ/g,'i').toLowerCase(),
+                    priceNew,
+                    imageUrl: imageUrlshort,
+                    link,
+                    timestamp: Date.now(),
+                    marka: 'addax',
+                }
+            } catch (error) {
+                return {error:error.toString(),content:document.innerHTML}
             }
+  
         })
     })
 
