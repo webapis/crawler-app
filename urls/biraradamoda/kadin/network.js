57649122a016229abf0f69dc81b9baadcabf5117
webapis <60083568+webapis@users.noreply.github.com>
2023-07-12 08:45:37 +0500

    Update network.js

--- urls/biraradamoda/kadin/network.js
+++ urls/biraradamoda/kadin/network.js
@@ -1,5 +1,6 @@
 const urls = [
-    {url:"https://www.network.com.tr/canta-731"}
+    {url:"https://www.network.com.tr/canta-731"},
+    {url:'https://www.network.com.tr/cuzdan-1321'}
     //    { url: 'https://www.network.com.tr/kadin-ust-giyim-689' },
     //    { url: 'https://www.network.com.tr/kadin-alt-giyim-690' },
     //    { url: 'https://www.network.com.tr/kadin-dis-giyim-691' },
