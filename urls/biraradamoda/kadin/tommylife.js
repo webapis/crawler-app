0017b5538b13b2921a0578cb9ffb0236167074be
webapis <60083568+webapis@users.noreply.github.com>
2023-08-11 10:07:05 +0500

    tommylife bugfix

--- urls/biraradamoda/kadin/tommylife.js
+++ urls/biraradamoda/kadin/tommylife.js
@@ -4,6 +4,22 @@ const urls = [
    { url: 'https://www.tommylife.com.tr/kadin-spor-alt-giyim'},
    { url: 'https://www.tommylife.com.tr/kadin-spor-takimlar'},
 
+   { url: 'https://www.tommylife.com.tr/erkek-ust-giyim'},
+   { url: 'https://www.tommylife.com.tr/erkek-alt-giyim'},
+   { url: 'https://www.tommylife.com.tr/erkek-esofman-takimlari'},
+
+   { url: 'https://www.tommylife.com.tr/erkek-cocuk-spor-ust-giyim'},
+   { url: 'https://www.tommylife.com.tr/erkek-cocuk-spor-alt-giyim'},
+   { url: 'https://www.tommylife.com.tr/erkek-cocuk-spor-takimlar'},
+
+
+   { url: 'https://www.tommylife.com.tr/kiz-cocuk-spor-ust-giyim'},
+   { url: 'https://www.tommylife.com.tr/kiz-cocuk-spor-alt-giyim'},
+   { url: 'https://www.tommylife.com.tr/kiz-cocuk-spor-takimlar'},
+
+   { url: 'https://www.tommylife.com.tr/erkek-spor-ayakkabi'},
+   { url: 'https://www.tommylife.com.tr/kadin-spor-ayakkabi'},
+
 
 ]
 
