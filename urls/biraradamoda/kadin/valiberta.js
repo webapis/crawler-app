fa4fd73e748d9ef549c8989e734c43a1f197fc85
webapis <60083568+webapis@users.noreply.github.com>
2023-08-11 09:46:31 +0500

    valiberta bug fix

---
+++ urls/biraradamoda/kadin/valiberta.js
@@ -0,0 +1,9 @@
+const urls = [
+    { url: 'https://www.valiberta.com/ust-giyim'},
+    { url: 'https://www.valiberta.com/dis-giyim'},
+    { url: 'https://www.valiberta.com/alt-giyim'},
+    { url: 'https://www.valiberta.com/yeni-sezon'},
+]
+
+module.exports={urls}
+
