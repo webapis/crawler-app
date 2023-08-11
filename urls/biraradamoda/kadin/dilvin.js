d6d4b3b3779dcbfc3a86188068f762b564620c81
webapis <60083568+webapis@users.noreply.github.com>
2023-08-11 13:47:27 +0500

    dilvin

--- urls/biraradamoda/kadin/dilvin.js
+++ urls/biraradamoda/kadin/dilvin.js
@@ -1,10 +1,28 @@
 const urls = [
 
-    { url: 'https://www.dilvin.com.tr/ust-giyim' },
-    { url: 'https://www.dilvin.com.tr/alt-giyim' },
-    { url: 'https://www.dilvin.com.tr/dis-giyim' },
-    { url: 'https://www.dilvin.com.tr/spor-giyim' },
-
+    { url: 'https://www.dilvin.com.tr/kadin-triko-modelleri' },
+    { url: 'https://www.dilvin.com.tr/bayan-elbise-modelleri' },
+    { url: 'https://www.dilvin.com.tr/crop-top' },
+    { url: 'https://www.dilvin.com.tr/bodysuit' },
+    { url: 'https://www.dilvin.com.tr/bustiyer' },
+    { url: 'https://www.dilvin.com.tr/kadin-bluz-modelleri' },
+    { url: 'https://www.dilvin.com.tr/atlet' },
+    { url: 'https://www.dilvin.com.tr/kadin-tisort-modelleri' },
+    { url: 'https://www.dilvin.com.tr/kadin-gomlek-modelleri' },
+    { url: 'https://www.dilvin.com.tr/kadin-sweatshirt-modelleri' },
+    { url: 'https://www.dilvin.com.tr/kadin-pantolon-modelleri' },
+    { url: 'https://www.dilvin.com.tr/etek-modelleri' },
+    { url: 'https://www.dilvin.com.tr/tulum-salopet' },
+    { url: 'https://www.dilvin.com.tr/sort-modelleri' },
+    { url: 'https://www.dilvin.com.tr/tayt-modelleri' },
+    { url: 'https://www.dilvin.com.tr/jean-pantolon-modelleri' },
+    { url: 'https://www.dilvin.com.tr/kadin-ceket-modelleri' },
+    { url: 'https://www.dilvin.com.tr/kadin-mont-modelleri' },
+    { url: 'https://www.dilvin.com.tr/bayan-kaban-modelleri' },
+    { url: 'https://www.dilvin.com.tr/kadin-hirka-modelleri' },
+    { url: 'https://www.dilvin.com.tr/trenckot' },
+    { url: 'https://www.dilvin.com.tr/kadin-yelek-modelleri' },
+   
 
 
 
