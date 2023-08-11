85d5ba64bec29156dc9a0f0d61ee39b387150382
webapis <60083568+webapis@users.noreply.github.com>
2023-08-11 09:04:37 +0500

    vitrin

--- handlers/biraradamoda/vitrin.js
+++ handlers/biraradamoda/vitrin.js
@@ -1,58 +1,60 @@
-
 async function handler(page, context) {
+  const url = await page.url();
+  debugger;
+  await page.waitForSelector(".fl.col-12.catalogWrapper");
+  const products = await page.evaluate(() => window.PRODUCT_DATA);
 
+  debugger;
 
-    const url = await page.url()
-debugger
-    await page.waitForSelector('.fl.col-12.catalogWrapper')
-    const products = await page.evaluate(()=>window.PRODUCT_DATA)
+  const data = products.map((document) => {
+    try {
+      const longImage = document.image;
+      const title = document.name;
+      const priceNew = document.total_sale_price.toString().replace(".", ",");
+      const link = document.url;
 
-    debugger;
+      return {
+        title: "vitrin " + title.replace(/İ/g, "i").toLowerCase(),
+        priceNew, //:priceNew.replace('.','').replace(',','.').trim(),
+        imageUrl: longImage, //.substring(longImage.indexOf('https://www.vitrin.com.tr') + 25),
+        link,
+        timestamp: Date.now(),
+        marka: "vitrin",
+      };
+    } catch (error) {
+      return { error: error.toString(), content: document.innerHTML };
+    }
+  });
 
-   const data = products.map(product => {
-   
-            const longImage =product.image
-            const title = product.name
-            const priceNew = product.total_sale_price.toString().replace('.',',')
-            const link = product.url
-  
-            return {
-                title:'vitrin '+title.replace(/İ/g,'i').toLowerCase(),
-                priceNew,//:priceNew.replace('.','').replace(',','.').trim(),
-                imageUrl: longImage.substring(longImage.indexOf('https://www.vitrin.com.tr') + 25),
-                link,
-                timestamp: Date.now(),
-                marka: 'vitrin',
+  console.log("data length_____", data.length, "url:", url);
 
-            }
-        })
-
-
-    console.log('data length_____', data.length, 'url:', url)
-
-    return data.map(m=>{return {...m,title:m.title+" _"+process.env.GENDER }})
+  return data.map((m) => {
+    return { ...m, title: m.title + " _" + process.env.GENDER };
+  });
 }
 
 async function getUrls(page) {
-debugger
-    const url = await page.url()
-    await page.waitForSelector('.productPager')
+  debugger;
+  const url = await page.url();
+  await page.waitForSelector(".productPager");
 
-    const totalPages = await page.evaluate(()=>Math.max(...Array.from(document.querySelectorAll('.productPager a[title]')).map(m=>m.getAttribute('title').replace(/[^\d]/g,'')).filter(Number).map(m=>parseInt(m))))
-    debugger
-    const pageUrls = []
+  const totalPages = await page.evaluate(() =>
+    Math.max(
+      ...Array.from(document.querySelectorAll(".productPager a[title]"))
+        .map((m) => m.getAttribute("title").replace(/[^\d]/g, ""))
+        .filter(Number)
+        .map((m) => parseInt(m))
+    )
+  );
+  debugger;
+  const pageUrls = [];
 
-    let pagesLeft = totalPages
-    for (let i = 2; i <= totalPages; i++) {
+  let pagesLeft = totalPages;
+  for (let i = 2; i <= totalPages; i++) {
+    pageUrls.push(`${url}?pg=` + i);
+    --pagesLeft;
+  }
 
-     
-
-        pageUrls.push(`${url}?pg=` + i)
-        --pagesLeft
-    
-
-    }
-
-    return { pageUrls, productCount:0, pageLength: pageUrls.length + 1 }
+  return { pageUrls, productCount: 0, pageLength: pageUrls.length + 1 };
 }
-module.exports = { handler, getUrls }
+module.exports = { handler, getUrls };
