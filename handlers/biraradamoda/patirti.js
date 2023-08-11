5aea1e8134b603ac71b02205bfc9457f15e23674
webapis <60083568+webapis@users.noreply.github.com>
2023-08-11 11:21:15 +0500

    patirti bugfix

--- handlers/biraradamoda/patirti.js
+++ handlers/biraradamoda/patirti.js
@@ -1,5 +1,5 @@
 
-var convert = require('xml-js');
+
 const { RequestQueue  } =require ('crawlee');
 
 async function handler(page, context) {
@@ -8,48 +8,65 @@ async function handler(page, context) {
     let data = []
     const url = await page.url()
 
-    await page.waitForSelector('#Katalog')
-    await page.waitForSelector('.productItem')
-    const productItems = await page.evaluate(() => document.querySelectorAll('.productItem').length)
-    const noMoreProducts = await page.evaluate(() => document.querySelector(".noMoreProducts").style.display === 'none')
+    let productItems =[]
+   
     debugger
-    if (productItems >= 20 && start) {
-
+    if (start) {
+     
+        const productExist = await page.$('.productItem')
+        if(productExist){
+         productItems = await page.evaluate(() => document.querySelectorAll('.productItem').length)
+        }
+    if(productItems.length>20){
         await requestQueue.addRequest({ url: `${url}?page=2`, userData: { start: false } })
+    }
+   
 
-    } else if (productItems >= 20 && !start) {
+    } else if (!start) {
+        const productExist = await page.$('.productItem')
+        if(productExist){
+            productItems = await page.evaluate(() => document.querySelectorAll('.productItem').length)
+            const currentPage = url.substring(url.indexOf('='))
+            const nextPage = parseInt(url.substring(url.indexOf('=') + 1)) + 1
+            const nextUrl = url.replace(currentPage, `=${nextPage}`)
+            debugger
+            await requestQueue.addRequest({ url: nextUrl, userData: { start: false } })
+        }
         debugger
-        const currentPage = url.substring(url.indexOf('='))
-        const nextPage = parseInt(url.substring(url.indexOf('=') + 1)) + 1
-        const nextUrl = url.replace(currentPage, `=${nextPage}`)
-        debugger
-        await requestQueue.addRequest({ url: nextUrl, userData: { start: false } })
+      
     }
 
     if (productItems > 0) {
-        data = await page.$$eval('.productItem', (productCards, _subcategory, _category, _opts, _node) => {
-            return productCards.map(productCard => {
-                const priceNew = productCard.querySelector('.currentPrice') ? productCard.querySelector('.currentPrice').textContent.replace(/\n/g, '').replace('₺', '').trim() : productCard.querySelector('.addPriceDiscount span').textContent.replace('₺', '').trim()
-                const longlink = productCard.querySelector('a.fl').href
-                const link = longlink.substring(longlink.indexOf("https://www.patirti.com/") + 24)
-                const longImgUrl = productCard.querySelector('img').src ? productCard.querySelector('img').src : productCard.querySelector('img[data-bind]').src
-                const imageUrlshort = longImgUrl && longImgUrl.substring(longImgUrl.indexOf("https://images.patirti.com/") + 27)
-                const title = productCard.querySelector('m[data-bind]').innerHTML
-                return {
-                    title: 'patirti ' + title.replace(/İ/g, 'i').toLowerCase(),
-                    priceNew,
-                    imageUrl: imageUrlshort, // imageUrlshort,
-                    link,
-
-                    timestamp: Date.now(),
-
-                    marka: 'patirti',
-
-
-
-                }
-            }).filter(f=>f.priceNew)
+        data = await page.$$eval('.productItem', (productCards) => {
+            return productCards.map(document => {
+                    try {
+                        const priceNew = document.querySelector('.currentPrice') ? document.querySelector('.currentPrice').textContent.replace(/\n/g, '').replace('₺', '').trim() : document.querySelector('.addPriceDiscount span').textContent.replace('₺', '').trim()
+                        const longlink = document.querySelector('a.fl').href
+                        const link = longlink.substring(longlink.indexOf("https://www.patirti.com/") + 24)
+                        const longImgUrl = document.querySelector('img').src ? document.querySelector('img').src : document.querySelector('img[data-bind]').src
+                       // const imageUrlshort = longImgUrl && longImgUrl.substring(longImgUrl.indexOf("https://images.patirti.com/") + 27)
+                        const title = document.querySelector('m[data-bind]').innerHTML
+                        return {
+                            title: 'patirti ' + title.replace(/İ/g, 'i').toLowerCase(),
+                            priceNew,
+                            imageUrl: longImgUrl,// imageUrlshort, // imageUrlshort,
+                            link,
+        
+                            timestamp: Date.now(),
+        
+                            marka: 'patirti',
+        
+        
+        
+                        }
+                    } catch (error) {
+                        return {error:error.toString(),content:document.innerHTML}
+                    }
+          
+            })//.filter(f=>f.priceNew)
         })
+
+
     }
 
 
