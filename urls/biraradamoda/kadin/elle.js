cf9553664a43770402572010e352561e30dad3d3
webapis <60083568+webapis@users.noreply.github.com>
2023-08-12 11:06:54 +0500

    tamertanca

--- urls/biraradamoda/kadin/elle.js
+++ urls/biraradamoda/kadin/elle.js
@@ -7,6 +7,16 @@ const urls = [
     { url: 'https://www.elleshoes.com/kadin-mini-canta' },
     { url: 'https://www.elleshoes.com/kadin-cuzdan' },
 
+    { url: 'https://www.elleshoes.com/kadin-spor-ayakkabi' },
+    { url: 'https://www.elleshoes.com/kadin-sandalet' },
+    { url: 'https://www.elleshoes.com/kadin-terlik' },
+    { url: 'https://www.elleshoes.com/kadin-loafer-ayakkabi' },
+    { url: 'https://www.elleshoes.com/kadin-topuklu-ayakkabi' },
+    { url: 'https://www.elleshoes.com/kadin-babet' },
+    { url: 'https://www.elleshoes.com/kadin-oxford-ayakkabi' },
+    { url: 'https://www.elleshoes.com/kadin-espadril' },
+    { url: 'https://www.elleshoes.com/kadin-bot' },
+    { url: 'https://www.elleshoes.com/kadin-cizme' },
 ]
 
 module.exports = { urls }
--- urls/biraradamoda/kadin/pierrecardin.js
+++ urls/biraradamoda/kadin/pierrecardin.js
@@ -1,6 +1,8 @@
 
 const urls = [{ url: 'https://www.pierrecardin.com.tr/kadin-canta-modelleri'},
-{url:"https://www.pierrecardin.com.tr/valiz"}
+{url:"https://www.pierrecardin.com.tr/valiz"},
+{url:"https://www.pierrecardin.com.tr/kadin"},
+
 
 ]
 
