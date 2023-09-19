22b12e0d8a38acd1d0de141ecc3d4147987bdb25
webapis <60083568+webapis@users.noreply.github.com>
2023-09-19 15:16:49 +0500

    divarese

--- handlers/biraradamoda/divarese.js
+++ handlers/biraradamoda/divarese.js
@@ -1,11 +1,7 @@
 
-async function handler(page) {
+async function extractor(page) {
 
-    debugger
-        const url = await page.url()
-    
-        await page.waitForSelector('#products')
-    debugger
+ 
     
         const data = await page.$$eval('.products__item', (productCards) => {
             return productCards.map(document => {
@@ -13,15 +9,13 @@ async function handler(page) {
                     const imageUrl = Array.from(document.querySelectorAll(".product__imageList img")).map(m=>m.getAttribute("data-original"))[0]
                     const title = document.querySelector("a.product__imageWrapper").getAttribute("title").trim()
                     const priceNew = document.querySelector(".product__price.-actual").innerText.replace("TL",'').trim()
-                    const longlink = document.querySelector("a.product__imageWrapper").href
-                    const link = longlink.substring(longlink.indexOf("https://www.divarese.com.tr/") + 28)
-                   
-                    const imageUrlshort = imageUrl && imageUrl.substring(imageUrl.indexOf("https://img-divarese.mncdn.com/") + 31)
-       
+                    const link = document.querySelector("a.product__imageWrapper").href
+                 
+              
                    return {
                         title: 'divarese ' + title,
                        priceNew,
-                        imageUrl: imageUrlshort,
+                        imageUrl,
                        link,
                         timestamp: Date.now(),
                         marka: 'divarese',
@@ -32,37 +26,33 @@ async function handler(page) {
               
             })
         })
-    debugger
-        console.log('data length_____', data.length, 'url:', url,process.env.GENDER)
-    
-    
-        console.log("process.env.GENDER ")
-        const formatprice = data.map((m) => {
-            return { ...m, title: m.title + " _" + process.env.GENDER }
-        })
-    
-    
-        return formatprice
+ return data
     }
     
     async function getUrls(page) {
         const url = await page.url()
-        await page.waitForSelector('.js-total-products-count')
-        const productCount = await page.$eval('.js-total-products-count', element => parseInt(element.innerHTML))
-        const totalPages = Math.ceil(productCount / 60)
+    const nextPage =    await page.$('.js-total-products-count')
+    let productCount =0
         const pageUrls = []
-    
-        let pagesLeft = totalPages
-        for (let i = 2; i <= totalPages; i++) {
-    
-    
-    
-            pageUrls.push(`${url}?page=` + i)
-            --pagesLeft
-    
-    
+        if(nextPage){
+             productCount = await page.$eval('.js-total-products-count', element => parseInt(element.innerHTML))
+            const totalPages = Math.ceil(productCount / 60)
+            for (let i = 2; i <= totalPages; i++) {
+                pageUrls.push(`${url}?page=` + i)
+            }
         }
+    
+     
     
         return { pageUrls, productCount, pageLength: pageUrls.length + 1 }
     }
-    module.exports = { handler, getUrls }
+    const productPageSelector='#products'
+    const linkSelector='.navbar__content a'
+    const linksToRemove=[]
+    const hostname='https://www.divarese.com.tr/'
+    const exclude=[]
+    const postFix =''
+    
+    module.exports = { extractor, getUrls,productPageSelector,linkSelector,linksToRemove,hostname ,exclude,postFix }
+    
+    
