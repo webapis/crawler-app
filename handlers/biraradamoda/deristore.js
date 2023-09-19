e317277bf411d0f7f2a87ffde29219a6d232b628
webapis <60083568+webapis@users.noreply.github.com>
2023-09-19 14:41:38 +0500

    desa

--- handlers/biraradamoda/deristore.js
+++ handlers/biraradamoda/deristore.js
@@ -1,64 +1,64 @@
 
 
-async function handler(page, context) {
-
-
-    const url = await page.url()
-
-    await page.waitForSelector('.product-grid .product')
-
+async function extractor(page) {
 
     const data = await page.$$eval('.product', (productCards) => {
         return productCards.map(document => {
+try {
+    const imageUrl = document.querySelector('.image img').src
+    const title = document.querySelector('.image img').alt.trim()
+    const priceNew = document.querySelector('.price').innerText.replace('₺', '').replace('TL', '').replace('$', '')+',00'
+    const link = document.querySelector('.image a').href
+ 
 
-            const imageUrl = document.querySelector('.image img').src
-            const title = document.querySelector('.image img').alt.trim()
-            const priceNew = document.querySelector('.price').innerText.replace('₺', '').replace('TL', '').replace('$', '')+',00'
-            const longlink = document.querySelector('.image a').href
-            const link = longlink.substring(longlink.indexOf('https://www.deristore.com.tr/') + 29)
-     
-            const imageUrlshort = imageUrl && imageUrl.substring(imageUrl.indexOf('https://www.deristore.com.tr/') + 29)
+    return {
+        title: 'deristore ' + title.replace(/İ/g,'i').toLowerCase(),
+        priceNew,
+        imageUrl,
+        link,
+        timestamp: Date.now(),
+        marka: 'deristore',
+    } 
+} catch (error) {
+    return{
+        error:error.toString(),content:document.innerHTML
+    }
+}
+        
+        })
 
-            return {
-                title: 'deristore ' + title.replace(/İ/g,'i').toLowerCase(),
-                priceNew,
-                imageUrl: imageUrlshort,
-                link,
-                timestamp: Date.now(),
-                marka: 'deristore',
-            }
-        }).filter(f => f.imageUrl && f.title.length > 5)
     })
 
-    console.log('data length_____', data.length, 'url:', url,process.env.GENDER)
+return data
 
-    console.log("process.env.GENDER ")
-    const formatprice = data.map((m) => {
-        return { ...m, title: m.title + " _" + process.env.GENDER }
-    })
 
-debugger
-    return formatprice
 }
 
 async function getUrls(page) {
     const url = await page.url()
-    await page.waitForSelector('.pagination a')
+    const pageUrls = []
+   const nextPage = await page.$('.pagination a')
+
+if(nextPage){
     const totalPages = await page.evaluate(()=>Math.max(...Array.from(document.querySelectorAll('.pagination a')).map(m=>m.innerText).filter(Number)))
     
-    const pageUrls = []
-
-    let pagesLeft = totalPages
     for (let i = 2; i <= totalPages; i++) {
 
-
-
         pageUrls.push(`${url}?page=` + i)
-        --pagesLeft
-
-
+    
     }
+}
+
+
 
     return { pageUrls, productCount:0, pageLength: pageUrls.length + 1 }
 }
-module.exports = { handler, getUrls }
+const productPageSelector='.product-grid .product'
+const linkSelector=''
+const linksToRemove=[]
+const hostname='https://www.deristore.com.tr/'
+const exclude=[]
+const postFix =''
+
+module.exports = { extractor, getUrls,productPageSelector,linkSelector,linksToRemove,hostname ,exclude,postFix }
+
--- handlers/biraradamoda/desa.js
+++ handlers/biraradamoda/desa.js
@@ -1,85 +1,55 @@
 
 
+const {autoScroll}=require('../../utils/autoscroll')
+async function extractor(page) {
 
-async function handler(page) {
-
-
-
-    await page.waitForSelector('.list-content')
-
+ 
     await autoScroll(page)
     debugger
     const data = await page.$$eval('.product-item-box', (productCards) => {
         return productCards.map(document => {
+try {
+    const title = document.querySelector('.product-name a').innerText
+    const imageUrl= document.querySelector('.product-item-image').getAttribute('data-src')
+    const priceNew =document.querySelector('.product-sale-price').innerText.replace('TL','').trim()
+    const link = document.querySelector('.product-name a').href
 
-            const title = document.querySelector('.product-name a').innerText
-            const img= document.querySelector('.product-item-image').getAttribute('data-src')
-            const priceNew =document.querySelector('.product-sale-price').innerText.replace('TL','').trim()
-            const link = document.querySelector('.product-name a').href
+    return {
+        title: 'desa '+title.replace(/İ/g,'i').replaceAll('-',' ').toLowerCase(),
+        priceNew,
+        imageUrl,
+        link,
+        timestamp: Date.now(),
+        marka: 'desa',
+    } 
 
-            return {
-                title: 'desa '+title.replace(/İ/g,'i').replaceAll('-',' ').toLowerCase(),
-                priceNew,//:priceNew.replace(',','.'),
-                imageUrl: img.substring(img.indexOf('https://14231c.cdn.akinoncloud.com/')+35),
-                link:link.substring(link.indexOf('https://www.desa.com.tr/')+24),
-                timestamp: Date.now(),
-                marka: 'desa',
+} catch (error) {
+    return{
+        error:error.toString(),content:document.innerHTML
+    }
+}
 
-            }
         })
     })
 
-
-
-    const formatprice = data.map((m) => {
-        return { ...m, title: m.title + " _" + process.env.GENDER }
-    })
-
-debugger
-    return formatprice
-
-
+return data
 
 
 }
 
-
-
-
-
-async function autoScroll(page) {
-    await page.evaluate(async () => {
-
-
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 100;
-            let inc = 0
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-                const range = Array.from(document.querySelector('.js-pagination-count.pagination-info div').innerText.split(' ')).filter(Number)
-                const total = range[0]
-                const collected = range[1]
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-                inc = inc + 1
-                if (total === collected) {
-                    clearInterval(timer);
-                    resolve();
-                } else {
-                    document.querySelector('.js-show-more.list-show-more').click()
-                }
-            }, 200);
-        });
-    });
-}
 
 async function getUrls(page) {
 
     const pageUrls = []
 
-
-
     return { pageUrls, productCount: 0, pageLength: pageUrls.length + 1 }
 }
-module.exports = { handler, getUrls }
+const productPageSelector='.list-content'
+const linkSelector='.navigation-wrapper a'
+const linksToRemove=[]
+const hostname='https://www.desa.com.tr/'
+const exclude=[]
+const postFix =''
+
+module.exports = { extractor, getUrls,productPageSelector,linkSelector,linksToRemove,hostname ,exclude,postFix }
+
