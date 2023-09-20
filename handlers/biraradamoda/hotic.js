cfb38c216b5213d43b099c8178a84f611247fffe
webapis <60083568+webapis@users.noreply.github.com>
2023-09-20 11:40:01 +0500

    hotic

--- handlers/biraradamoda/hotic.js
+++ handlers/biraradamoda/hotic.js
@@ -1,8 +1,6 @@
 
-async function handler(page, context) {
+async function extractor(page) {
 
-    debugger
-    const url = await page.url()
 
     await page.waitForSelector('.item-grid')
     debugger
@@ -13,32 +11,23 @@ async function handler(page, context) {
              const imageUrl =document.querySelector('[data-original]').getAttribute('data-original')
             const title = document.querySelector('.product__inside__name a').innerText
              const priceNew = document.querySelector('.product-price-general').innerText.replace('TL','').trim()
-               const longlink = document.querySelector('.product__inside__name a').href
-             const link = longlink.substring(longlink.indexOf("https://www.hotic.com.tr/")+25)
-  
-            const imageUrlshort = imageUrl && imageUrl.substring(imageUrl.indexOf("https://img2-hotic.mncdn.com/") + 29)
-
+               const link = document.querySelector('.product__inside__name a').href
+    
             return {
-                title:'hotic ' + title,
+                 title:'hotic ' + title,
                  priceNew,
-             imageUrl: imageUrlshort,
-                  link,
+                 imageUrl,
+                link,
                 timestamp: Date.now(),
                 marka: 'hotic',
             }
-        })//.filter(f => f.imageUrl !== null && f.title.length > 5)
+        })
     })
     debugger
-    console.log('data length_____', data.length, 'url:', url, process.env.GENDER)
 
 
-    console.log("process.env.GENDER ")
-    const formatprice = data.map((m) => {
-        return { ...m, title: m.title.replaceAll('(',' ').replaceAll(')',' ') + " _" + process.env.GENDER }
-    })
 
-
-    return formatprice
+    return data
 }
 
 async function getUrls(page) {
@@ -65,4 +54,12 @@ async function getUrls(page) {
 
     return { pageUrls, productCount: 0, pageLength: pageUrls.length + 1 }
 }
-module.exports = { handler, getUrls }
+const productPageSelector='.item-grid'
+const linkSelector='nav.navbar a'
+const linksToRemove=[]
+const hostname='https://www.hotic.com.tr/'
+const exclude=[]
+const postFix =''
+
+module.exports = { extractor, getUrls,productPageSelector,linkSelector,linksToRemove,hostname ,exclude,postFix }
+
