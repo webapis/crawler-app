4165744a0bf2fd4056a3cf4caa19683abb3650a1
webapis <60083568+webapis@users.noreply.github.com>
2023-09-20 08:54:53 +0500

    dd

--- handlers/biraradamoda/gizia.js
+++ handlers/biraradamoda/gizia.js
@@ -1,19 +1,29 @@
 
-async function handler(page) {
+async function extractor(page) {
 
 
-    const url = await page.url()
-debugger
+
     await page.waitForSelector('.fl.col-12.catalogWrapper')
     debugger
     await page.hover('.countryChange')
-    await page.click('[data-value=TL]')
-    await page.waitForNavigation()
+
     debugger
     await page.waitForSelector('.countryChange')
      await page.hover('.countryChange')
-     await page.click('[data-value=tr]')
-     await page.waitForNavigation()
+
+     const valueExits  =   await page.$('[data-value=tr]')
+     const valueTLExits  =   await page.$('[data-value=TL]')
+     if(valueExits){
+        console.log('chaged language')
+        await page.click('[data-value=tr]')
+        await page.waitForNavigation()
+     }
+     if(valueTLExits){
+        console.log('chaged to TL')
+        await page.click('[data-value=TL]')
+        await page.waitForNavigation()
+     }
+  
     debugger
     const products = await page.evaluate(()=>window.PRODUCT_DATA)
 
@@ -21,15 +31,15 @@ debugger
 
    const data = products.map(product => {
    
-            const longImage =product.image
+            const imageUrl =product.image
             const title = product.name
             const priceNew = product.total_sale_price.toString().replace('.',',')
             const link = product.url
   
             return {
                 title:'gizia '+title.replace(/İ/g,'i').toLowerCase(),
-                priceNew,//:priceNew.replace('.','').replace(',','.').trim(),
-                imageUrl: longImage.substring(longImage.indexOf('https://www.gizia.com') + 21),
+                priceNew,
+                imageUrl,
                 link,
                 timestamp: Date.now(),
                 marka: 'gizia',
@@ -38,34 +48,37 @@ debugger
         })
 
 
-    console.log('data length_____', data.length, 'url:', url)
 
-    return data.map(m=>{return {...m,title:m.title+" _"+process.env.GENDER }})
+    return data
 }
 
 async function getUrls(page) {
     debugger
     const url = await page.url()
-    await page.waitForSelector('.productPager')
+  const nextPage =  await page.$('.productPager')
 
+
+    const pageUrls = []
+if(nextPage){
     const totalPages = await page.evaluate(()=>Math.max(...Array.from(document.querySelectorAll('.productPager a[title]')).map(m=>m.getAttribute('title').replace(/[^\d]/g,'')).filter(Number).map(m=>parseInt(m))))
     debugger
-    const pageUrls = []
-
-    let pagesLeft = totalPages
     for (let i = 2; i <= totalPages; i++) {
-
-     
-
         pageUrls.push(`${url}?pg=` + i)
-        --pagesLeft
-    
-
     }
+}
+ 
 
     return { pageUrls, productCount:0, pageLength: pageUrls.length + 1 }
  
  
    
 }
-module.exports = { handler, getUrls }
+const productPageSelector='.fl.col-12.catalogWrapper'
+const linkSelector='nav ul.menu a'
+const linksToRemove=[]
+const hostname='https://www.gizia.com'
+const exclude=[]
+const postFix =''
+
+module.exports = { extractor, getUrls,productPageSelector,linkSelector,linksToRemove,hostname ,exclude,postFix }
+
---
+++ urls/biraradamoda/all/gizia.js
@@ -0,0 +1,9 @@
+const urls = [
+  { url: 'https://www.gizia.com/'},
+
+
+
+]
+
+module.exports = { urls }
+
