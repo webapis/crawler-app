08ebd50e9a366c4f9fb8a00cc81ab36d928d274d
webapis <60083568+webapis@users.noreply.github.com>
2023-09-20 15:33:06 +0500

    Update kigili.js

--- handlers/biraradamoda/kigili.js
+++ handlers/biraradamoda/kigili.js
@@ -61,7 +61,7 @@ async function getUrls(page) {
     return { pageUrls, productCount: 0, pageLength: pageUrls.length + 1 }
 }
 const productPageSelector='.list-content'
-const linkSelector='a:not(.product-item-wrapper a)'
+const linkSelector='.ddd'
 const linksToRemove=[]
 const hostname='https://www.kigili.com/'
 const exclude=[]
