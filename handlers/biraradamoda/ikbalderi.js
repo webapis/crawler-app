3aec36eced7c22ccff5f398cccaaf45266f96996
webapis <60083568+webapis@users.noreply.github.com>
2023-09-20 12:07:17 +0500

    ikbalderi

--- handlers/biraradamoda/ikbalderi.js
+++ handlers/biraradamoda/ikbalderi.js
@@ -1,12 +1,6 @@
 
 
-async function handler(page, context) {
-
-
-    const url = await page.url()
-
-    await page.waitForSelector('.products')
-
+async function extractor(page) {
 
     const data = await page.$$eval('.product-small', (productCards) => {
         return productCards.map(document => {
@@ -14,13 +8,13 @@ async function handler(page, context) {
                 const imageUrl =   document.querySelector('img[data-srcset]').getAttribute('data-srcset').split(', ')[0].split(' ')[0]
                 const title = document.querySelector('.product-title a').text.trim()
                 const priceNew = document.querySelector('.woocommerce-Price-amount.amount bdi').textContent.trim().replace('₺', '')
-                const longlink = document.querySelector('.product-title a').href
-                const link = longlink.substring(longlink.indexOf("https://ikbalderi.com/") + 22)
-                const imageUrlshort = imageUrl && imageUrl.substring(imageUrl.indexOf("https://ikbalderi.com/") + 22)
+                const link = document.querySelector('.product-title a').href
+              
+  
                 return {
                     title: 'ikbalderi ' + title.replace(/İ/g, 'i').toLowerCase(),
                     priceNew,
-                    imageUrl: imageUrlshort,
+                    imageUrl,
                     link,
                     timestamp: Date.now(),
                     marka: 'ikbalderi',
@@ -29,38 +23,40 @@ async function handler(page, context) {
                 return {error:error.toString(),content:document.innerHTML}
             }
         
-        })//.filter(f => f.imageUrl !== null && f.title.length > 10)
+        })
     })
 
-    console.log('data length_____', data.length, 'url:', url, process.env.GENDER)
+return data
 
-    debugger
-    console.log("process.env.GENDER ")
-    const mapgender = data.map((m) => {
-        return { ...m,  title: m.title + " _" + process.env.GENDER }
-    })
-
-    return mapgender
+   
 }
 
 async function getUrls(page) {
     const url = await page.url()
-    await page.waitForSelector('.woocommerce-result-count.hide-for-medium')
-    const productCount = await page.evaluate(() => parseInt(document.querySelector('.woocommerce-result-count.hide-for-medium').innerText.split(' ')[0]))
-    const totalPages = Math.ceil(productCount / 12)
+ const nextPage =   await page.$('.woocommerce-result-count.hide-for-medium')
+ let productCount = 0
+
     const pageUrls = []
-
-    let pagesLeft = totalPages
+if(nextPage){
+    productCount = await page.evaluate(() => parseInt(document.querySelector('.woocommerce-result-count.hide-for-medium').innerText.split(' ')[0]))
+    const totalPages = Math.ceil(productCount / 12)
     for (let i = 2; i <= totalPages; i++) {
-
-
-
+        
         pageUrls.push(`${url}/page/` + i + '/')
-        --pagesLeft
-
-
+      
     }
+}
+
+  
 
     return { pageUrls, productCount: 0, pageLength: pageUrls.length + 1 }
 }
-module.exports = { handler, getUrls }
+const productPageSelector='.products'
+const linkSelector='#woocommerce_product_categories-13 a'
+const linksToRemove=[]
+const hostname='https://ikbalderi.com/'
+const exclude=[]
+const postFix =''
+
+module.exports = { extractor, getUrls,productPageSelector,linkSelector,linksToRemove,hostname ,exclude,postFix }
+
