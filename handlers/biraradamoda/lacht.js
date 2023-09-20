fc063b6ab894b8810fb6b7008461f4a64371e6e8
webapis <60083568+webapis@users.noreply.github.com>
2023-09-20 16:20:34 +0500

    lacht

--- handlers/biraradamoda/lacht.js
+++ handlers/biraradamoda/lacht.js
@@ -1,25 +1,17 @@
 
+//const {autoScroll}=require('../../utils/autoscroll')
+async function extractor(page) {
 
-async function handler(page, context) {
-
-
-    const url = await page.url()
-
-    await page.waitForSelector('.pattern-group-body')
-
-    await autoScroll(page)
+   // await autoScroll(page)
     const data = await page.$$eval('.card-product', (productCards) => {
         return productCards.map(document => {
             try {
               
-            const imageUrl = document.querySelector('.image img').src
+            const imageUrl = document.querySelector('img[onmouseover]').getAttribute('onmouseover').replace('this.src=','')
             const title = document.querySelector('.card-product-inner .title').innerText.trim()
             const priceNew = document.querySelector('.sale-price').innerText.replace('TL', '').trim()
-            const longlink = document.querySelector('a.c-p-i-link').href
-            const link = longlink.substring(longlink.indexOf("https://www.lacht.com.tr") + 24)
-
-          //  const imageUrlshort = imageUrl && imageUrl.substring(imageUrl.indexOf("https://cdn.qukasoft.com") + 24)
-
+            const link = document.querySelector('a.c-p-i-link').href
+         
             return {
                 title: 'lacht ' + title.replace(/İ/g, 'i').toLowerCase(),
                 priceNew,
@@ -32,42 +24,12 @@ async function handler(page, context) {
                 return {error:error.toString(),content:document.innerHTML}
             }
 
-        })//.filter(f => f.imageUrl !== null && f.title.length > 5)
+        })
     })
 
-    console.log('data length_____', data.length, 'url:', url, process.env.GENDER)
-
-
-    console.log("process.env.GENDER ")
-    const mapgender = data.map((m) => {
-        return { ...m, title: m.title + " _" + process.env.GENDER }
-    })
-
-
-    return mapgender
+return data
 }
-async function autoScroll(page) {
-    await page.evaluate(async () => {
 
-
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 100;
-            let inc = 0
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-                var toth = 7775
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-                inc = inc + 1
-                if (totalHeight >= scrollHeight - window.innerHeight) {
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 200);
-        });
-    });
-}
 async function getUrls(page) {
     const url = await page.url()
     const nextPageExist = await page.$('.pagination .page-link')
@@ -76,17 +38,10 @@ async function getUrls(page) {
 
         const totalPages = await page.evaluate(() => Math.max(...Array.from(document.querySelectorAll('.pagination .page-link')).map(m => m.innerText).filter(Number)))
 
-
-
-        let pagesLeft = totalPages
         for (let i = 2; i <= totalPages; i++) {
 
-
-
             pageUrls.push(`${url}?sayfa=` + i)
-            --pagesLeft
-
-
+           
         }
 
     }
@@ -94,4 +49,11 @@ async function getUrls(page) {
 
     return { pageUrls, productCount: 0, pageLength: pageUrls.length + 1 }
 }
-module.exports = { handler, getUrls }
+const productPageSelector='.pattern-group-body'
+const linkSelector='section.menu a'
+const linksToRemove=[]
+const hostname='https://www.lacht.com.tr/'
+const exclude=[]
+const postFix =''
+
+module.exports = { extractor, getUrls,productPageSelector,linkSelector,linksToRemove,hostname ,exclude,postFix }
