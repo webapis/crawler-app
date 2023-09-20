0949669c569f5b987b2426b7f83f233eeab067e6
webapis <60083568+webapis@users.noreply.github.com>
2023-09-20 13:49:14 +0500

    kigili

--- handlers/biraradamoda/kigili.js
+++ handlers/biraradamoda/kigili.js
@@ -1,8 +1,7 @@
 
-async function handler(page, context) {
 
-debugger
-    await page.waitForSelector('.list-content')
+const {autoScroll}=require('../../utils/autoscroll')
+async function extractor(page) {
 
    await autoScroll(page)
     debugger
@@ -10,15 +9,15 @@ debugger
         return productCards.map(document => {
                 try {
                     const title = document.querySelector('.product-name a').innerText
-                    const img= document.querySelector(".product-item-image-link [data-default-img]")?document.querySelector(".product-item-image-link [data-default-img]").getAttribute('data-default-img'):(document.querySelector(".product-item-image-link .product-item-image")?document.querySelector(".product-item-image-link .product-item-image").src: null)
+                    const imageUrl= document.querySelector(".product-item-image-link [data-default-img]")?document.querySelector(".product-item-image-link [data-default-img]").getAttribute('data-default-img'):(document.querySelector(".product-item-image-link .product-item-image")?document.querySelector(".product-item-image-link .product-item-image").src: null)
                     const priceNew =document.querySelector('.product-sale-price').innerText.replace('TL','').trim()
                     const link = document.querySelector('.product-name a').href
-        
+    
                     return {
                         title: 'kigili '+title.replace(/İ/g,'i').replaceAll('-',' ').toLowerCase(),
-                        priceNew,//:priceNew.replace(',','.'),
-                        imageUrl:img,//&& img.substring(img.indexOf('https://kigili.akinoncdn.com/')+29),
-                        link:link.substring(link.indexOf('https://www.kigili.com/')+23),
+                        priceNew,
+                        imageUrl,
+                        link,
                         timestamp: Date.now(),
                         marka: 'kigili',
         
@@ -30,14 +29,7 @@ debugger
         })
     })
 debugger
-
-console.log('data length_____', data.length, 'url:', url)
-    const formatprice = data.map((m) => {
-        return { ...m, title: m.title + " _" + process.env.GENDER }
-    })
-
-debugger
-    return formatprice//.filter(f => f.imageUrl !== null  && f.imageUrl.length>0 )
+return data
 
 
 
@@ -47,48 +39,32 @@ debugger
 
 
 
-async function autoScroll(page) {
-    await page.evaluate(async () => {
 
-
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 100;
-            let inc = 0
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-                inc = inc + 1
-                if (totalHeight >= scrollHeight - window.innerHeight) {
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 100);
-        });
-    });
-}
 
 async function getUrls(page) {
 
     const url = await page.url()
-    await page.waitForSelector('.pagination .pagination-item')
-    const totalPages = await page.evaluate(() => Math.max(...Array.from(document.querySelectorAll(".pagination .pagination-item")).map(m=>m.innerText.trim()).filter(Number)))
+   const nextPage = await page.$('.pagination .pagination-item')
 
     const pageUrls = []
+    if(nextPage){
+        const totalPages = await page.evaluate(() => Math.max(...Array.from(document.querySelectorAll(".pagination .pagination-item")).map(m=>m.innerText.trim()).filter(Number)))
 
-    let pagesLeft = totalPages
-    for (let i = 2; i <= totalPages; i++) {
-
-
-
-        pageUrls.push(`${url}?page=` + i)
-        --pagesLeft
-
-
+        for (let i = 2; i <= totalPages; i++) {
+            pageUrls.push(`${url}?page=` + i)
+         
+        }
     }
+
+  
 
     return { pageUrls, productCount: 0, pageLength: pageUrls.length + 1 }
 }
-module.exports = { handler, getUrls }
+const productPageSelector='.list-content'
+const linkSelector='a:not(.product-item-image-link)'
+const linksToRemove=[]
+const hostname='https://www.kigili.com/'
+const exclude=[]
+const postFix =''
+
+module.exports = { extractor, getUrls,productPageSelector,linkSelector,linksToRemove,hostname ,exclude,postFix }
---
+++ urls/biraradamoda/all/kigili.js
@@ -0,0 +1,7 @@
+const urls = [
+    { url: 'https://www.kigili.com/takim-elbise/'},
+ 
+  ]
+  
+  module.exports = { urls }
+  
