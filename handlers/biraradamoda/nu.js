0a1cde4ab178ca896c534ecec595362f499b8628
webapis <60083568+webapis@users.noreply.github.com>
2023-09-25 17:05:31 +0500

    nu

--- handlers/biraradamoda/nu.js
+++ handlers/biraradamoda/nu.js
@@ -1,69 +1,49 @@
 
-async function handler(page, context) {
+//const {autoScroll}=require('../../utils/autoscroll')
+const initValues ={
+  productPageSelector:'#CollectionProductGrid',
+  linkSelector:'.main-navigation a',
+  linksToRemove:[],
+  hostname:'https://www.nu.com.tr/',
+  exclude:[],
+  postFix:''
+}
+
+async function extractor(page) {
 
 
     const url = await page.url()
 
-    await page.waitForSelector('#CollectionProductGrid')
-    await autoScroll(page)
+   // await autoScroll(page)
 
-    const data = await page.$$eval('article.product-item', (productCards) => {
+    const data = await page.$$eval('article.product-item', (productCards,url) => {
         return productCards.map(document => {
-
-            const imageUrl =document.querySelector('.product-item-image[data-srcset]')&&  document.querySelector('.product-item-image').getAttribute('data-srcset').split(',').reverse()[0].trim()
-            const title = document.querySelector('.product--item-title a').innerText
-            const priceNew = Array.from(document.querySelector('.product-item-price').querySelectorAll('span')).map(m=>m.innerText.replace('₺','').trim()).sort().reverse()[0]
-            const longlink = document.querySelector('.product--item-title a').href
-            const link = longlink.substring(longlink.indexOf("https://www.nu.com.tr/") + 22)
-         //   const longImgUrl = imageUrl && imageUrl.substring(imageUrl.indexOf('//cdn.shopify.com/')+18)
-          //  const imageUrlshort = imageUrl && longImgUrl.substring(longImgUrl.indexOf("//") + 1)
-
-            return {
-                title: 'nu ' + title.replace(/İ/g,'i').toLowerCase(),
-                priceNew,
-                imageUrl,
-                link,
-                timestamp: Date.now(),
-                marka: 'nu',
+            try {
+                const imageUrl =document.querySelector('.product-item-image[data-srcset]')&&  document.querySelector('.product-item-image').getAttribute('data-srcset').split(',').reverse()[0].trim()
+                const title = document.querySelector('.product--item-title a').innerText
+                const priceNew =0// Array.from(document.querySelector('.product-item-price').querySelectorAll('span')).map(m=>m.innerText.replace('₺','').trim()).sort().reverse()[0]
+                const link = document.querySelector('.product--item-title a').href
+           
+                return {
+                    title: 'nu ' + title.replace(/İ/g,'i').toLowerCase(),
+                    priceNew,
+                    imageUrl,
+                    link,
+                    timestamp: Date.now(),
+                    marka: 'nu',
+                }  
+            } catch (error) {
+                return {error:error.toString(),url,content:document.innerHTML}
             }
-        }).filter(f => f.imageUrl !== null && f.title.length > 5)
-    })
+      
+        })
+    },url)
 
-    console.log('data length_____', data.length, 'url:', url,process.env.GENDER)
-
-debugger
-    console.log("process.env.GENDER ")
-    const formatprice = data.map((m) => {
-        return { ...m, title: m.title + " _" + process.env.GENDER }
-    })
-
-
-    return formatprice
+return data
 }
 
 
-async function autoScroll(page) {
-    await page.evaluate(async () => {
 
-
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 100;
-            let inc = 0
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-                inc = inc + 1
-                if (totalHeight >= scrollHeight - window.innerHeight) {
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 200);
-        });
-    });
-}
 
 async function getUrls(page) {
     const url = await page.url()
@@ -91,4 +71,4 @@ async function getUrls(page) {
 
     return { pageUrls, productCount:0, pageLength: pageUrls.length + 1 }
 }
-module.exports = { handler, getUrls }
+module.exports = { extractor, getUrls,...initValues }
