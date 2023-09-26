eab17a13c54946977a22d344cccaacd2f97da664
webapis <60083568+webapis@users.noreply.github.com>
2023-09-26 11:58:17 +0500

    panco

--- handlers/biraradamoda/panco.js
+++ handlers/biraradamoda/panco.js
@@ -1,62 +1,63 @@
 
 
-async function handler(page, context) {
+const initValues ={
+    productPageSelector:'.js-list-products',
+    linkSelector:'.navigation a',
+    linksToRemove:[],
+    hostname:'https://www.panco.com.tr/',
+    exclude:[],
+    postFix:''
+  }
+
+async function extractor(page) {
 
 
     const url = await page.url()
 
-    await page.waitForSelector('.js-list-products')
 
-
-    const data = await page.$$eval('.product-item-box', (productCards) => {
+    const data = await page.$$eval('.product-item-box', (productCards,url) => {
         return productCards.map(productCard => {
+try {
+    const imageUrl = productCard.querySelector('.product-item-image').src
+    const title = productCard.querySelector('.product-name').innerHTML.replace(/\n/g, '').trim()
+    const priceNew = productCard.querySelector('.product-sale-price').innerHTML.replace(/\n/g, '').replace("TL",'').trim()
+   const link = productCard.querySelector('.product-item-info a').href
 
-            const imageUrl = productCard.querySelector('.product-item-image').src
-             const title = productCard.querySelector('.product-name').innerHTML.replace(/\n/g, '').trim()
-             const priceNew = productCard.querySelector('.product-sale-price').innerHTML.replace(/\n/g, '').replace("TL",'').trim()
-            const longlink = productCard.querySelector('.product-item-info a').href
-            const link = longlink.substring(longlink.indexOf("https://www.panco.com.tr/") + 25)
-            // const longImgUrl = imageUrl && 'https:' + imageUrl.substring(imageUrl.lastIndexOf('//'), imageUrl.lastIndexOf('.jpg') + 4)
-            const imageUrlshort = imageUrl.substring(imageUrl.indexOf("https://cdn-panco.akinon.net/products/") + 38)
-
-            return {
-                title: 'panco ' + title.replace(/İ/g,'i').toLowerCase(),
-                 priceNew,
-                 imageUrl: imageUrlshort,
-                 link,
-                timestamp: Date.now(),
-                marka: 'panco',
-            }
-        }).filter(f => f.imageUrl !== null && f.title.length > 10)
-    })
+   return {
+       title: 'panco ' + title.replace(/İ/g,'i').toLowerCase(),
+        priceNew,
+        imageUrl,
+        link,
+       timestamp: Date.now(),
+       marka: 'panco',
+   } 
+} catch (error) {
+    return {error:error.toString(),url,content:document.innerHTML}
+}
+          
+        })
+    },url)
 debugger
-    console.log('data length_____', data.length, 'url:', url,process.env.GENDER)
-
-
-    console.log("process.env.GENDER ")
-    const formatprice = data.map((m) => {
-        return { ...m, title: m.title + " _" + process.env.GENDER }
-    })
-
-
-    return formatprice
+return data
 }
 
 async function getUrls(page) {
     const url = await page.url()
-    await page.waitForSelector('.js-pagination')
-    const totalPages = await page.evaluate(()=>document.querySelectorAll(".js-pagination")[document.querySelectorAll(".js-pagination").length-1])
-    
+  const nextPage =  await page.$('.js-pagination')
+ 
     const pageUrls = []
 
-    let pagesLeft = totalPages
+ if(nextPage){
+    const totalPages = await page.evaluate(()=>document.querySelectorAll(".js-pagination")[document.querySelectorAll(".js-pagination").length-1])
+    
     for (let i = 2; i <= totalPages; i++) {
-
         pageUrls.push(`${url}?page=` + i)
-        --pagesLeft
-
     }
+ }
+ 
 
     return { pageUrls, productCount:0, pageLength: pageUrls.length + 1 }
 }
-module.exports = { handler, getUrls }
+module.exports = { extractor, getUrls,...initValues }
+
+
