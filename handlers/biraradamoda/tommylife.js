494448a0bb0f7ae90cddd14c2e47671d4697f247
Serdar Ashirov <webapis@users.noreply.github.com>
2023-09-27 09:30:41 +0500

    tommylife

--- handlers/biraradamoda/tommylife.js
+++ handlers/biraradamoda/tommylife.js
@@ -1,90 +1,67 @@
 
 
-async function handler(page, context) {
+const initValues ={
+    productPageSelector:'.catalogWrapper',
+    linkSelector:'#mainMenu a',
+    linksToRemove:[],
+    hostname:'https://www.tommylife.com.tr/',
+    exclude:[],
+    postFix:''
+  }
+async function extractor(page) {
 
 
     const url = await page.url()
 
-    await page.waitForSelector('.catalogWrapper')
-    await page.waitForSelector('span b')
+   // await page.waitForSelector('.catalogWrapper')
+   // await page.waitForSelector('span b')
 
 
   //  await autoScroll(page)
-    const data = await page.$$eval('.productItem', (productCards) => {
+    const data = await page.$$eval('.productItem', (productCards,url) => {
         return productCards.map(document => {
             try {
                 const imageUrl = document.querySelector('span[itemprop="image"]').getAttribute('content')
                 const title = document.querySelector('.productItem a[title]').innerText
                 const priceNew = document.querySelector('.currentPrice')? document.querySelector('.currentPrice').innerHTML.replace('TL', '').trim():null
-                const longlink = document.querySelector('.productItem a[title]').href
-             const link = longlink.substring(longlink.indexOf("https://www.tommylife.com.tr/") + 29)
-    
-               // const imageUrlshort = imageUrl && imageUrl.substring(imageUrl.indexOf("https://cdn.tommylife.com.tr/") + 29)
-    
+                const link = document.querySelector('.productItem a[title]').href
+       
+        
                 return {
                     title: 'tommylife ' + title.replace(/İ/g, 'i').toLowerCase(),
                     priceNew,
-                    imageUrl, //imageUrlshort,
+                    imageUrl,
                     link,
                     timestamp: Date.now(),
                     marka: 'tommylife',
                 }
             } catch (error) {
-                return {error:error.toString(),content:document.innerHTML}
+                return {error:error.toString(),url,content:document.innerHTML}
             }
       
-        }).filter(f => f.imageUrl !== null && f.title.length > 3 && f.priceNew != null)
-    })
+        })
+    },url)
 
-    console.log('data length_____', data.length, 'url:', url, process.env.GENDER)
-
-
-    console.log("process.env.GENDER ")
-
-    const formatprice = data.map((m) => {
-        return { ...m, title: m.title + " _" + process.env.GENDER }
-    })
-
-
-    return formatprice
+return data
 }
 
 
 
 
-async function autoScroll(page) {
-    await page.evaluate(async () => {
 
-
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 100;
-            let inc = 0
-            var timer = setInterval(() => {
-              
-                const totalCollected = document.querySelectorAll('.productItem').length
-                const totalItems = parseInt(document.querySelector('.uluLeft b').innerText)
-
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-                inc = inc + 1
-                if (totalCollected === totalItems) {
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 250);
-        });
-    });
-}
 async function getUrls(page) {
   const url = await page.url()
-      await page.waitForSelector('span b')
-     const productCount = await page.$eval('span b', element => parseInt(element.innerText))
-    const totalPages =  Math.round(productCount/4) //await page.evaluate(() => Math.max(...Array.from(document.querySelectorAll('.page_numbers span')).map(m => m.innerHTML).filter(Number)))
-    const pageUrls = []
-    pageUrls.push(`${url}?ps=` + totalPages)
+  const nextPage=    await page.$('span b')
+ 
+      const pageUrls = []
+    if(nextPage){
+        const productCount = await page.$eval('span b', element => parseInt(element.innerText))
+    const totalPages =  Math.round(productCount/4)
+        pageUrls.push(`${url}?ps=` + totalPages)
+    }
+ 
 
 
     return { pageUrls, productCount: 0, pageLength: pageUrls.length + 1 }
 }
-module.exports = { handler, getUrls }
+module.exports = { extractor, getUrls,...initValues }
