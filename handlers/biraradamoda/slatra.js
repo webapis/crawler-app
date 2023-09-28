706661039db7c1e1d0eb6a79060a9d9a3020b31f
webapis <60083568+webapis@users.noreply.github.com>
2023-09-28 14:25:45 +0500

    slatra

--- handlers/biraradamoda/slatra.js
+++ handlers/biraradamoda/slatra.js
@@ -1,73 +1,52 @@
-
-async function handler(page, context) {
+const {autoScroll}=require('../../utils/autoscroll')
+const initValues ={
+    productPageSelector:'#ProductListMainContainer',
+    linkSelector:'.navigation a',
+    linksToRemove:[],
+    hostname:'https://www.slatra.com.tr/',
+    exclude:[],
+    postFix:''
+  }
+async function extractor(page, ) {
 
     debugger;
     const url = await page.url()
-    // const acceptcookies = await page.$('.insider-opt-in-notification-button.insider-opt-in-disallow-button')
-    // if (acceptcookies) {
-    //     await page.click('.insider-opt-in-notification-button.insider-opt-in-disallow-button')
-    // }
-    await page.waitForSelector('#ProductListMainContainer')
-
+  
+  
      await autoScroll(page)
     debugger;
 
 
-    const data = await page.$$eval('.ItemOrj', (productCards) => {
+    const data = await page.$$eval('.ItemOrj', (productCards,url) => {
         try {
             return productCards.map(document => {
                 const title = document.querySelector(".productName.detailUrl a").innerHTML
-                const img =document.querySelector("a .productSliderImage")&& document.querySelector("a .productSliderImage").src
+                const imageUrl =document.querySelector("a .productSliderImage")&& document.querySelector("a .productSliderImage").src
                 const priceNew = document.querySelector(".discountPrice span").innerText.replace('₺', '')
                 const link = document.querySelector(".productName.detailUrl a").href
     
                 return {
                     title: 'slatra ' + title.replace(/İ/g, 'i').toLowerCase(),
-                    priceNew: priceNew,//.replace(',','.'),
-                    imageUrl: img, // && img.substring(img.indexOf('https://static.ticimax.cloud/') + 29),
-                    link: link.substring(link.indexOf('https://www.slatra.com.tr/') + 26),
+                    priceNew: priceNew,
+                    imageUrl, 
+                    link,
                     timestamp: Date.now(),
                     marka: 'slatra',
     
                 }
             }) 
         } catch (error) {
-            return {error:error.toString(),content:document.innerHTML}
+            return {error:error.toString(),url,content:document.innerHTML}
         }
       
-    })
-    console.log('data length_____', data.length, 'url:', url)
-    debugger
-
-    return data.map(m => { return { ...m, title: m.title + " _" + process.env.GENDER } })//.//filter(f => f.imageUrl !== null && f.title.length > 5)
+    },url)
+return data
 }
 
 
 
 
 
-async function autoScroll(page) {
-    await page.evaluate(async () => {
-
-
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 100;
-            let inc = 0
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-                var toth = 7775
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-                inc = inc + 1
-                if (totalHeight >= scrollHeight - window.innerHeight) {
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 200);
-        });
-    });
-}
 async function getUrls(page) {
 
     const url = await page.url()
@@ -80,13 +59,12 @@ async function getUrls(page) {
         const totalPages = Math.ceil(productCount / 50)
 
 
-        let pagesLeft = totalPages
         for (let i = 2; i <= totalPages; i++) {
 
 
 
             pageUrls.push(`${url}?sayfa=` + i)
-            --pagesLeft
+          
 
 
         }
@@ -95,4 +73,4 @@ async function getUrls(page) {
 
     return { pageUrls, productCount: 0, pageLength: pageUrls.length + 1 }
 }
-module.exports = { handler, getUrls }
+module.exports = { extractor, getUrls,...initValues }
